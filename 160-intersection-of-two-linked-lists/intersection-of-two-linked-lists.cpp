206fd3749ddf9fda3c46a8aac8bb57ca374c37be
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-03-18 00:53:24 +0530

    Time: 34 ms (77.82%) | Memory: 18.5 MB (82.14%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -4,39 +4,39 @@ public:
         ListNode* tempA=headA;
         ListNode* tempB=headB;
         int lenA=0;
-        while(tempA!=NULL){
+        while(tempA!=NULL){// to find len of a;
             lenA++;
             tempA=tempA->next;
         }
         int lenB=0;
-        while(tempB!=NULL){
+        while(tempB!=NULL){//to find len of b;
             lenB++;
             tempB=tempB->next;
         }
         tempA=headA;
         tempB=headB;
         int diff=0;
-        if(lenA>lenB){
+        if(lenA>lenB){//if a is greater
             diff=lenA-lenB;
             for(int i=1;i<=diff;i++){
                 tempA=tempA->next;
             }
-            while(tempA!=tempB){
+            while(tempA!=tempB){//check when they are equal or we can say intersect
                 tempA=tempA->next;
                 tempB=tempB->next;
             }
-            return tempA;
+            return tempA;//or tempb;
         }
-        else{
+        else{//if b is less than or equal to a;
             diff=lenB-lenA;
             for(int i=1;i<=diff;i++){
                 tempB=tempB->next;
             }
-            while(tempA!=tempB){
+            while(tempA!=tempB){//check when they are equal or we can say intersect
                 tempA=tempA->next;
                 tempB=tempB->next;
             }
-            return tempA;
+            return tempA;//or tempb
         }
     }
 };
