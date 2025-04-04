a9246589a05a356880f468358b3840c9e9269b4a
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-04-04 12:37:52 +0530

    Time: 8 ms (80.76%) | Memory: 11.8 MB (53.47%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,3 +11,16 @@ public:
         return false;
     }
 };
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast) return true;
+//         }
+//         return false;
+//     }
+// };
