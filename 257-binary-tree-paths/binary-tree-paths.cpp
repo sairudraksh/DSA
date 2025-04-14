28d4655e05981b64c85f6719ea213b9962a14ae6
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-04-14 16:03:35 +0530

    Time: 4 ms (14.63%) | Memory: 17.8 MB (38.26%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void treepath(vector<string>&s,string st,TreeNode* root){
         if(root==NULL) return;
-        st+=to_string(root->val);
-        if(root->left!=NULL || root->right!=NULL) st+="->";
-        if(root->left==NULL && root->right==NULL){
+        st+=to_string(root->val);//just add to string
+        if(root->left!=NULL || root->right!=NULL) st+="->";// add this everytime expect at leaf(last) node
+        if(root->left==NULL && root->right==NULL){//push ant ans when we are as last or leaf node
             s.push_back(st);
             return;
         }
