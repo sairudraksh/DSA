6a5967b03128bccfd094334857c4aba62a7f8e16
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-04-18 16:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.98%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public: 
+    int count(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+count(root->left)+count(root->right);
+    }
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return NULL;
+        return count(root);
+    }
+};
