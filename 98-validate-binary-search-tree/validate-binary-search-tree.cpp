6d57834f16a9607b3b6378ff6f78513a10526aee
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-04-19 20:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (94.99%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    void validate(TreeNode* root,vector<int>&v){
+    TreeNode* prev=NULL;
+    bool flag=true;
+    void validate(TreeNode* root){
         if(root==NULL) return;
-        validate(root->left,v);
-        v.push_back(root->val);
-        validate(root->right,v);
-    }
-    bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        validate(root,v);
-        int n=v.size();
-        for(int i=0;i<n-1;i++){
-            if(v[i]>=v[i+1]){
-                return false;
+        validate(root->left);
+        if(prev!=NULL){
+            if(root->val<=prev->val){
+                flag=false;
+                return;
             }
         }
-        return true;
+        prev=root;
+        validate(root->right);
+    }
+    bool isValidBST(TreeNode* root) {
+        validate(root);
+        return flag;
     }
 };
