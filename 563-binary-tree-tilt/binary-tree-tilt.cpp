8be09ab54d1b937c43e44efa92e781452ec3ecaa
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-04-24 14:47:10 +0530

    Time: 10 ms (6.68%) | Memory: 23.7 MB (77.50%) - LeetSync

--- 563-binary-tree-tilt/binary-tree-tilt.cpp
+++ 563-binary-tree-tilt/binary-tree-tilt.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int summ(TreeNode* root,int &sum){
+    int summ(TreeNode* root,int &sum){//to find sum for absolute difference and return one side value
         if(root==NULL) return 0;
         sum=sum+root->val;
         summ(root->left,sum);
         summ(root->right,sum);
         return sum;
     }
-    void sol(TreeNode* root){
+    void sol(TreeNode* root){// to put new values in trees
         if(root==NULL) return;
         int suma=0;
         int sumb=0;
@@ -15,7 +15,7 @@ public:
         sol(root->left);
         sol(root->right);
     }
-    int findsum(TreeNode* root,int &sum){
+    int findsum(TreeNode* root,int &sum){//to find sum
         if(root==NULL) return 0;
         sum=sum+root->val;
         findsum(root->left,sum);
