9b91bd2a616afc8e553e6486edc23a00903555aa
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-05-15 09:54:20 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.14%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int>map;
+        for(int i=0;i<text.length();i++){
+            map[text[i]]++;
+        }
+        int count=0;
+        while(!map.empty()){
+            string s="balloon";
+            for(int i=0;i<s.length();i++){
+                int flagg=0;
+                for(auto x:map){
+                    if(x.first==s[i]){
+                        flagg=1;
+                        break;
+                    }
+                }
+                if(flagg==1) map[s[i]]--;
+                if(map[s[i]]==0) map.erase(s[i]);
+                if(flagg==0) return count;
+            }
+            count++;
+        }
+        return count;
+    }
+};
