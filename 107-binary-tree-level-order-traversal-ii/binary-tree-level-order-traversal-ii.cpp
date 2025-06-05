47cd35fd0317d96a1d7231769f97da6c8207d8bb
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-06 03:08:45 +0530

    Time: 2 ms (23.60%) | Memory: 16.1 MB (25.64%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.cpp
@@ -1,26 +1,36 @@
+// class Solution {
+// public:
+//     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        
+//     }
+// };
 class Solution {
 public:
-    int levels(TreeNode* root){
-        if(root==NULL) return 0;
-        return 1+max(levels(root->left),levels(root->right));
-    }
-    void find(TreeNode* root,int curr,int target,vector<int>&v){
+    void find(TreeNode* root,vector<int>&v,int curr,int target){
         if(root==NULL) return;
         if(curr==target){
             v.push_back(root->val);
             return;
         }
-        find(root->left,curr+1,target,v);
-        find(root->right,curr+1,target,v);
+        find(root->left,v,curr+1,target);
+        find(root->right,v,curr+1,target);
+    }
+    int levels(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+max(levels(root->left),levels(root->right));
     }
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        int n=levels(root);
         vector<vector<int>>v1;
-        if(root==NULL) return v1;
-        int curr=1;
-        for(int i=n;i>0;i--){
+        int n=levels(root);
+        if(n==1){
             vector<int>v;
-            find(root,curr,i,v);
+            v.push_back(root->val);
+            v1.push_back(v);
+            return v1;
+        }
+        for(int i=n;i>=1;i--){
+            vector<int>v;
+            find(root,v,1,i);
             v1.push_back(v);
         }
         return v1;
