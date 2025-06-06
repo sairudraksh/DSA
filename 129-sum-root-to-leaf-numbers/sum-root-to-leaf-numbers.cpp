0eafd682b156817e2dc21b8b9c5182d75ff7d2fc
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-06 23:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (71.73%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    void sum(TreeNode* root,vector<int>v,int &summ){
+    void find(TreeNode* root,int currsum,int &totalsum){
         if(root==NULL) return;
-        v.push_back(root->val);
+        currsum=currsum*10+root->val;
         if(root->left==NULL && root->right==NULL){
-            int n=v.size()-1;
-            long long k=1;
-            while(n>=0){
-                int num=v[n]*k;
-                summ=summ+num;
-                n--;
-                k=k*10;
-            }
-            return;
+            totalsum=totalsum+currsum;
         }
-        sum(root->left,v,summ);
-        sum(root->right,v,summ);
+        find(root->left,currsum,totalsum);
+        find(root->right,currsum,totalsum);
     }
     int sumNumbers(TreeNode* root) {
-        vector<int>v;
-        int summ=0;
-        sum(root,v,summ);
-        return summ;
+        if(root==NULL) return 0;
+        int currsum=0;
+        int totalsum=0;
+        find(root,currsum,totalsum);
+        return totalsum;
     }
 };
