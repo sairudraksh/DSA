5fa3dc81f8dd0f59e4a22daff6a78737f6272074
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-08 20:12:41 +0530

    Time: 11 ms (2.58%) | Memory: 22.6 MB (87.27%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    void find(TreeNode* root,int curr,int target,int &ans){
+        if(root==NULL) return;
+        if(curr==target){
+            if(root->val>ans){
+                ans=root->val;
+            }
+        }
+        find(root->left,curr+1,target,ans);
+        find(root->right,curr+1,target,ans);
+    }
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
-    void largest(TreeNode* root,int curr,int target,int &max){
-        if(root==NULL) return;
-        if(curr==target && root->val>max){
-            max=root->val;
-            return;
-        }
-        largest(root->left,curr+1,target,max);
-        largest(root->right,curr+1,target,max);
-    }
     vector<int> largestValues(TreeNode* root) {
+        int n=levels(root);
         vector<int>v;
-        if(root==NULL){
-            return v;
-        }
-        int n=levels(root); 
         for(int i=1;i<=n;i++){
-            int max=INT_MIN; 
-            largest(root,1,i,max);
+            int max=INT_MIN;
+            int curr=1;
+            find(root,curr,i,max);
             v.push_back(max);
         }
         return v;
