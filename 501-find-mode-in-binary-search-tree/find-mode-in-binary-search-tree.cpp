09e0b6407e554281d486022c2ff6d8e3ac63a022
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-08 19:11:41 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (38.96%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    unordered_map<int,int>map;
-    void find(TreeNode* root){
+    void find(TreeNode* root,unordered_map<int,int>&map){
         if(root==NULL) return;
         map[root->val]++;
-        find(root->left);
-        find(root->right);
+        find(root->left,map);
+        find(root->right,map);
     }
     vector<int> findMode(TreeNode* root) {
         vector<int>v;
         if(root==NULL) return v;
-        find(root);
+        unordered_map<int,int>map;
+        find(root,map);
         int max=INT_MIN;
-        for(auto x: map){
+        for(auto &x: map){
             if(x.second>max) max=x.second;
         }
-        for(auto x: map){
+        for(auto &x: map){
             if(x.second==max) v.push_back(x.first);
         }
         return v;
