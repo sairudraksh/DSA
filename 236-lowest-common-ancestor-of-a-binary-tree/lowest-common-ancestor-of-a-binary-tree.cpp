70c704eaebea8549422b4895a4ac3c7e84a1eda4
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-08 16:35:58 +0530

    Time: 890 ms (5.07%) | Memory: 17.5 MB (18.34%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    bool exists(TreeNode* root,TreeNode* target){
-        if(root==NULL) return false; 
-        if(root==target) return true;
-        return exists(root->left,target) || exists(root->right,target);
+    bool exists(TreeNode* root, TreeNode* p){
+        if(root==NULL) return false;
+        if(root->val==p->val) return true;
+        if(exists(root->left,p) || exists(root->right,p)) return true;
+        else return false;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==p || root==q) return root;
-        else if (exists(root->left,p) && exists(root->right,q)) return root;
-        else if (!exists(root->left,p) && !exists(root->right,q)) return root;
-        else if (exists(root->left,p) && !exists(root->right,q)) return lowestCommonAncestor(root->left,p,q);
+        if(root==NULL) root;
+        if(p==q) return p;
+        else if(root==p) return p;
+        else if(root==q) return q;
+        else if(exists(root->left,q) && exists(root->right,p)) return root;
+        else if(exists(root->left,p) && exists(root->right,q)) return root;
+        else if(exists(root->left,p) && exists(root->left,q)) return lowestCommonAncestor(root->left,p,q);
         else return lowestCommonAncestor(root->right,p,q);
-
     }
 };
