e0012128adcda607e5db71fd7eaa494932fa60da
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-08 23:01:43 +0530

    Time: 0 ms (100.00%) | Memory: 35.2 MB (24.98%) - LeetSync

--- 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    void greatestsum(TreeNode* root,int &sum){
+    void find(TreeNode* root,int &sum){
         if(root==NULL) return;
-        greatestsum(root->right,sum);
+        find(root->right,sum);
         root->val=root->val+sum;
         sum=root->val;
-        greatestsum(root->left,sum);
+        find(root->left,sum);
     }
     TreeNode* convertBST(TreeNode* root) {
         int sum=0;
-        greatestsum(root,sum);
+        find(root,sum);
         return root;
     }
 };
