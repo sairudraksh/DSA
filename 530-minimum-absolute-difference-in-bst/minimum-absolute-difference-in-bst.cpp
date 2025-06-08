003ecc022dd47305412d56797c32ad496d457626
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-08 22:22:49 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (27.00%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    void find(TreeNode* root,vector<int>&v){
+    void getMinimumDifference(TreeNode* root,vector<int>&v){
         if(root==NULL) return;
-        find(root->left,v);
+        getMinimumDifference(root->left,v);
         v.push_back(root->val);
-        find(root->right,v);
+        getMinimumDifference(root->right,v);
     }
     int getMinimumDifference(TreeNode* root) {
-        if(root==NULL) return 0;
-        int min=INT_MAX;
         vector<int>v;
-        find(root,v);
-        for(int i=0;i<v.size()-1;i++){
-            int ans=v[i+1]-v[i];
-            if(ans<0) ans=ans*-1;
-            if(ans<min){
-                min=ans;
-            }
+        getMinimumDifference(root,v);
+        int min=INT_MAX;
+        int n=v.size();
+        for(int i=1;i<n;i++){
+            int diff=abs(v[i]-v[i-1]);
+            if(diff<min) min=diff;
         }
         return min;
     }
