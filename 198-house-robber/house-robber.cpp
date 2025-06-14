3b9810ac34f24733ff916f9daf9d1aee0496fd0c
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-14 18:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (71.95%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    vector<int>dp;
-    int find(vector<int>&v,int i){
-        int n=v.size();
-        if(i==n-1) return v[n-1];
-        else if(i==n-2) return max(v[n-2],v[n-1]);
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=max(v[i]+find(v,i+2),find(v,i+1));
-    }
     int rob(vector<int>& nums) {
-        dp.assign(405, -1); //we cannot assign vector size and values outside function;
-        int idx=0;
-        int a=find(nums,idx);
-        return a;
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int>dp(405,-1);
+        dp[n-1]=nums[n-1];
+        dp[n-2]=max(nums[n-2],nums[n-1]);
+        for(int i=n-3;i>=0;i--){
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+        }
+        return dp[0];
     }
 };
