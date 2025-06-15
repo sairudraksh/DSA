9547b6ff4e4142574490fd258085b3264caec965
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-15 11:13:40 +0530

    Time: 19 ms (32.82%) | Memory: 21.2 MB (5.02%) - LeetSync

---
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,21 @@
+#define mod 1000000007
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int find(int n,int k,int target){
+        if(n==0 && target==0) return 1;
+        if(n==0) return 0;
+        if(dp[n][target]!=-1) return dp[n][target];
+        int sum=0;
+        for(int i=1;i<=k;i++){
+            if(i>target) continue;
+            sum=((sum%mod)+find(n-1,k,target-i)%mod)%mod;
+        }
+        return dp[n][target]=sum%mod;
+    }
+    int numRollsToTarget(int n, int k, int target) {
+        dp.clear();
+        dp.resize(35,vector<int>(1005,-1));
+        return find(n,k,target);
+    }
+};
