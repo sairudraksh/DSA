33615ee08cc211ac7c863d9c34310a64305e7e49
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-17 15:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.48%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int>dp;
     int find(int n){
-        if(n==1 || n==0) return 1;
-        if(n==2) return 2;
+        if(n==1 || n==0) return dp[n]=1;
+        if(n==2) return dp[n]=2;
         if(dp[n]!=-1) return dp[n];
         int sum=0;
         for(int k=1;k<=n;k++){
