26079df802eeaf6a4526a6003be9d5c34734924d
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-18 16:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (79.44%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    vector<int>dp;
     int find(int n){
-        if(n==0 || n==1) return 1;
-        if(n==2) return 2;
+        if(n==0 || n==1) return dp[n]=1;
+        if(n==2) return dp[n]=2;
+        if(dp[n]!=-1) return dp[n];
         int sum=0;
         for(int i=1;i<=n;i++){
             sum=sum+find(i-1)*find(n-i);
         }
-        return sum;
+        return dp[n]=sum;
     }
     int numTrees(int n) {
+        dp.resize(100,-1);
         return find(n);
     }
 };
