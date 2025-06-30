3d163607e1dcd8477e404b4bce9b336cabd4c057
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-30 12:10:18 +0530

    Time: 31 ms (11.21%) | Memory: 27 MB (15.32%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    int find(string &text1,string &text2,int idx1,int idx2,vector<vector<int>>&dp){// use & as it might give tle;
-        if(idx1==0 || idx2==0) return 0; // dont put dp[idx1][idx2] here
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(text1[idx1-1]==text2[idx2-1]) {
-            return dp[idx1][idx2]=1+find(text1,text2,idx1-1,idx2-1,dp);
+    vector<vector<int>>dp;
+    int find(string &s1,string &s2,int i,int j){
+        if(i<=0 || j<=0){
+            return 0;
         }
-        return dp[idx1][idx2]=max(find(text1,text2,idx1-1,idx2,dp),find(text1,text2,idx1,idx2-1,dp));
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i-1]==s2[j-1]){
+            return dp[i][j]=1+find(s1,s2,i-1,j-1);
+        }
+        else return dp[i][j]=max(find(s1,s2,i-1,j),find(s1,s2,i,j-1));
     }
-    
     string shortestCommonSupersequence(string s1, string s2) {
-        int n=s1.length();
-        int m=s2.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        int a=find(s1,s2,n,m,dp);
+        int n1=s1.length();
+        int n2=s2.length();
+        dp.resize(n1+1,vector<int>(n2+1,-1));
+        int a=find(s1,s2,n1,n2);
+        int i=n1;
+        int j=n2;
         string ans="";
-        int i=n;
-        int j=m;
         while(i>0 && j>0){
             if(s1[i-1]==s2[j-1]){
                 ans+=s1[i-1];
                 i--;
                 j--;
             }
-            else if(dp[i-1][j]>dp[i][j-1]){//upward
+            else if(dp[i-1][j]>dp[i][j-1]){
                 ans+=s1[i-1];
                 i--;
             }
