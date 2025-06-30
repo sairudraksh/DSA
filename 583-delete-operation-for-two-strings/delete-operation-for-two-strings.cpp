ef2f1c3b779f1dc5ea633acf5edc3e955f0bf832
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-06-30 10:55:38 +0530

    Time: 15 ms (31.05%) | Memory: 17.2 MB (12.56%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int find(string &text1,string &text2,int idx1,int idx2,vector<vector<int>>&dp){// use & as it might give tle;
-        if(idx1<0 || idx2<0) return 0; // dont put dp[idx1][idx2] here
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(text1[idx1]==text2[idx2]) {
-            return dp[idx1][idx2]=1+find(text1,text2,idx1-1,idx2-1,dp);
+    vector<vector<int>>dp;
+    int find(string &s1,string &s2,int &n1,int &n2,int i,int j){
+        if(i>=n1){
+            return (n2-j);
         }
-        return dp[idx1][idx2]=max(find(text1,text2,idx1-1,idx2,dp),find(text1,text2,idx1,idx2-1,dp));
+        if(j>=n2){
+            return (n1-i);
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]){
+            return dp[i][j]=find(s1,s2,n1,n2,i+1,j+1);
+        }
+        else return dp[i][j]=min(1+find(s1,s2,n1,n2,i+1,j),1+find(s1,s2,n1,n2,i,j+1));
     }
-    int minDistance(string &s1, string &s2) {
-        vector<vector<int>> dp(1001, vector<int>(1001,-1));
-        int idx1=s1.size()-1;
-        int idx2=s2.size()-1;
-        int lcs=find(s1,s2,idx1,idx2,dp);
-        return (s1.length() - lcs) + (s2.length() - lcs);
+    int minDistance(string s1, string s2) {
+        int n1=s1.length();
+        int n2=s2.length();
+        dp.resize(n1+1,vector<int>(n2+1,-1));
+        return find(s1,s2,n1,n2,0,0);
     }
 };
