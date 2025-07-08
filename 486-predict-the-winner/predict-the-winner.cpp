429294bd14e83dbf916232b7cf37828de114ef8e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-09 02:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (26.94%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int find(vector<int>&nums,int i,int j){
+    int find(vector<int>&nums,int n,int i,int j){
         if(i>j) return 0;
+        if(i==j) return nums[i];
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=max(nums[i]-find(nums,i+1,j),nums[j]-find(nums,i,j-1));
+        return dp[i][j]=max(nums[i]-find(nums,n,i+1,j),nums[j]-find(nums,n,i,j-1));
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return true;
         dp.resize(n+1,vector<int>(n+1,-1));
-        // if returned value is greater than 1 means alice have greater value
-        int a=find(nums,0,n-1);
-        if(a>=0) return true;
-        else return false;
+        int a=find(nums,n,0,n-1);
+        if(a<0) return false;
+        else return true;
     }
 };
