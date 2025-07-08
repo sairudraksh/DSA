521dfa67ddd2319996b4df650ff2dc59a5918a9e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-09 01:16:29 +0530

    Time: 76 ms (52.85%) | Memory: 105.8 MB (13.42%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    vector<long long>dp;
     vector<vector<long long>>jobs;
-    long long find(long long i){
+    vector<long long>dp;
+    int find(long long i){
         if(i>=jobs.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        long long l =i+1, r =jobs.size()-1, x = jobs.size();
-        while(l<=r) {
+        long long x=jobs.size();
+        long long l=i+1;
+        long long r=jobs.size()-1;
+        while(l<=r){
             long long mid=(r+l)/2;
-            if (jobs[mid][0]>=jobs[i][1]) {
+            if(jobs[mid][0]>=jobs[i][1]){
                 x=mid;
                 r=mid-1;
-            } else {
-                l=mid+1;
             }
+            else l=mid+1;
         }
-        return dp[i]=max(jobs[i][2]+find(x),find(i+1));
+        long long a=jobs[i][2]+find(x);
+        long long b=find(i+1);
+        return dp[i]=max(a,b);
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        long long n=endTime.size();
         jobs.clear();
-        for(long long i=0;i<n;i++){
-            jobs.push_back({(long long) startTime[i],(long long) endTime[i],(long long) profit[i]});
+        for(long long i=0;i<startTime.size();i++){
+            jobs.push_back({(long long)startTime[i],(long long)endTime[i],(long long)profit[i]});
         }
-        dp.resize(n+1,-1);
+        dp.resize(startTime.size()+1,-1);
         sort(jobs.begin(),jobs.end());
-        return (int)find(0);
+        return find(0);
     }
 };
