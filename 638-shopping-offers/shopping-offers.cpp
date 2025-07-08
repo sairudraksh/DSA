96d47c85340edf9d5fb33e7fd256ed5d5dd5eb19
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-09 01:56:40 +0530

    Time: 3 ms (89.04%) | Memory: 15.9 MB (73.48%) - LeetSync

--- 638-shopping-offers/shopping-offers.cpp
+++ 638-shopping-offers/shopping-offers.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
     map<vector<int>, int> dp;
-
-    int find(vector<int>& price, vector<vector<int>>& special, vector<int>& needs){
-        if (dp.count(needs)) return dp[needs];
-        bool tag=false;
+    int find(vector<int>&price,vector<vector<int>>&special,vector<int>needs){
+        if(dp.count(needs)) return dp[needs];
+        bool tag=true;
         for(int i=0;i<needs.size();i++){
             if(needs[i]!=0){
-                tag=true;
-                break;
+                tag=false;
             }
         }
-        if(tag==false) return 0;
+        if(tag==true) return 0;
         vector<int>v=needs;
-        int ans=INT_MAX;
+        int b=INT_MAX;
         for(int i=0;i<special.size();i++){
-            bool flagg=false;
+            bool flagg=true;
             for(int j=0;j<needs.size();j++){
-                if(special[i][j]>needs[j]){
-                    flagg=true;
+                if(needs[j]<special[i][j]){
+                    flagg=false;
                     break;
                 }
             }
-            if(flagg==false){
-                for(int j=0;j<needs.size();j++) {
-                    needs[j] -= special[i][j];
+            if(flagg==true){
+                for(int l=0;l<needs.size();l++){
+                    needs[l]-=special[i][l];
                 }
-                ans=min(ans,special[i][special[0].size()-1]+find(price,special,needs));
+                b=min(b,special[i][special[i].size()-1]+find(price,special,needs));
                 needs=v;
             }
         }
-        int ans1=0;
+        int ans=0;
         for(int i=0;i<needs.size();i++){
-            ans1+=needs[i]*price[i];// we directly bought all
+            ans+=needs[i]*price[i];
         }
-        return dp[needs]=min(ans1,ans);   
+        return dp[needs]=min(ans,b);
     }
     int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
-        
         return find(price,special,needs);
     }
 };
