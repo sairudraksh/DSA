d9e8f3e62d7c140d56a289e919472bc8e8be6cc9
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-14 22:49:25 +0530

    Time: 3 ms (42.44%) | Memory: 60.6 MB (75.11%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.cpp
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    void find(TreeNode* root,int curr,int target,int& sum){
+    int prevcurr=-1;
+    int sum=0;
+    void find(TreeNode* root,int curr){
         if(root==NULL) return;
-        if(curr==target){
-            sum=sum+root->val;
-            return;
+        if(curr==prevcurr){
+            sum+=root->val;
         }
-        find(root->left,curr+1,target,sum);
-        find(root->right,curr+1,target,sum);
-    }
-    int levels(TreeNode* root){
-        if(root==NULL) return 0;
-        return 1+max(levels(root->left),levels(root->right));
+        if(curr>prevcurr){
+            prevcurr=curr;
+            sum=0;
+            sum+=root->val;
+        }
+        find(root->left,curr+1);
+        find(root->right,curr+1);
     }
     int deepestLeavesSum(TreeNode* root) {
-        if(root==NULL) return 0;
-        int n=levels(root);
-        if(n==1) return root->val;
-        int sum=0;
-        int target=n;
-        int curr=1;
-        find(root,curr,target,sum);
+        find(root,1);
         return sum;
     }
 };
