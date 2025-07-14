9df1c648a09c989faa66f6513a80840a4581fe0d
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-15 01:49:33 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (9.65%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    void find(TreeNode* root,int a,int b,int &sum){
+    void find(TreeNode* root,int &low,int &high,int &ans){
         if(root==NULL) return;
-        if(root->val>=a && root->val<=b) sum=sum+root->val;
-        if(a<root->val && b<root->val) find(root->left,a,b,sum);
-        else if(a>root->val && b>root->val) find(root->right,a,b,sum);
-        else{
-            find(root->left,a,b,sum);
-            find(root->right,a,b,sum);
+        if(root->val>=low && root->val<=high){
+            ans+=root->val;
         }
+        find(root->left,low,high,ans);
+        find(root->right,low,high,ans);
     }
     int rangeSumBST(TreeNode* root, int low, int high) {
-        int sum=0;
-        find(root,low,high,sum);
-        return sum;
+        int ans=0;
+        find(root,low,high,ans);
+        return ans;
     }
 };
