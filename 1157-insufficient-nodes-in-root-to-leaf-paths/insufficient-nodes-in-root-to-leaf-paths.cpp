e0a17098b7c3f2fbea84c661b4984a0a6ff208e5
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-15 00:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 34.5 MB (38.81%) - LeetSync

--- 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.cpp
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    TreeNode* find(TreeNode* root,int sum,int limit){
+    TreeNode* find(TreeNode* root,int &limit,int sum){
         if(root==NULL) return NULL;
         sum+=root->val;
         if(root->left==NULL && root->right==NULL){
             if(sum>=limit) return root;
             else return NULL;
         }
-        root->left=find(root->left,sum,limit);
-        root->right=find(root->right,sum,limit);
-
-        if(root->left==NULL && root->right==NULL) return NULL;
+        root->left=find(root->left,limit,sum);
+        root->right=find(root->right,limit,sum);
+        if(root->left==NULL && root->right==NULL){
+            return NULL;
+        }
         return root;
     }
     TreeNode* sufficientSubset(TreeNode* root, int limit) {
-        return find(root,0,limit);
+        return find(root,limit,0);
     }
 };
