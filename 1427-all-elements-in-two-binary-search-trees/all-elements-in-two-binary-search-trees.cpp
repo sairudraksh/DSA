df7f047e78eab59c81cbfa8e930089a10b10463d
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-14 22:39:20 +0530

    Time: 3 ms (85.06%) | Memory: 86 MB (94.74%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void find(TreeNode* root ,vector<int>&v){
+    void find(TreeNode* root,vector<int>&v){
         if(root==NULL) return;
-        find(root->left,v);
         v.push_back(root->val);
+        find(root->left,v);
         find(root->right,v);
+
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int>v;
