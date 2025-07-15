b191b3928b9fcb889c89ab796aa45844b72127bd
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-16 02:21:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (6.20%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    void rightside(TreeNode* root,vector<int>&v){
-        if(root==NULL){
-            v.push_back(INT_MIN);
-            return;
-        }
-        v.push_back(root->val);
-        rightside(root->right,v);
-        rightside(root->left,v);
-    }
     void leftside(TreeNode* root,vector<int>&v){
         if(root==NULL){
-            v.push_back(INT_MIN);
+            v.push_back(-100);
             return;
         }
         v.push_back(root->val);
         leftside(root->left,v);
         leftside(root->right,v);
     }
+
+    void rightside(TreeNode* root,vector<int>&v){
+        if(root==NULL){
+            v.push_back(-100);
+            return;
+        }
+        v.push_back(root->val);
+        rightside(root->right,v);
+        rightside(root->left,v);
+    }
+
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL) return true;
-        if(root->left==NULL && root->right==NULL) return true;
         vector<int>v1;
-        vector<int>v2;
         leftside(root->left,v1);
+        vector<int>v2;
         rightside(root->right,v2);
-        int n1=v1.size();
-        int n2=v2.size();
-        if(n1!=n2) return false;
-        for(int i=0;i<n1;i++){
+        if(v1.size()!=v2.size()) return false;
+        for(int i=0;i<v1.size();i++){
             if(v1[i]!=v2[i]) return false;
         }
         return true;
