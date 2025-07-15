bad2ffd378479ccd6482bc176bd4ac9c7287bfd0
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-15 18:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (76.84%) - LeetSync

--- 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
+++ 671-second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    long long maxval=LLONG_MAX;
-    long long smaxval=LLONG_MAX;
+    long long maximum;
+    long long smax;
     void find(TreeNode* root){
         if(root==NULL) return;
-        if(root->val<maxval){
-            smaxval=maxval;
-            maxval=root->val;
+        if(root->val<maximum){ 
+            smax=maximum;
+            maximum=root->val;
         }
-        else if(root->val>maxval && root->val <smaxval) smaxval=root->val;
+        else if(root->val<smax && root->val>maximum && root->val!=maximum){
+            smax=root->val;
+        } 
         find(root->left);
         find(root->right);
     }
     int findSecondMinimumValue(TreeNode* root) {
+        maximum=LLONG_MAX;
+        smax=LLONG_MAX;
         find(root);
-        if(smaxval==LLONG_MAX) return -1;
-        return (int)smaxval;
+        if(smax==LLONG_MAX) return -1;
+        return (int)smax;
     }
 };
