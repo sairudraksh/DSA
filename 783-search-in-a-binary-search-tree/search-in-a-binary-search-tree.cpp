cda3ca64dd101f85eb09c1611cfe0c91703defde
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-15 17:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (27.15%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -5,7 +5,5 @@ public:
         else if(root->val==val) return root;
         else if(root->val>val) return searchBST(root->left,val);
         else return searchBST(root->right,val);
-
-
     }
 };
