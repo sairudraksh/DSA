5493f76388a354c5b8c5f59dcfa1cba42d07bcb0
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-16 02:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (80.99%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int n=arr.size();
-        int m=arr[0].size();
-        if (arr[0][0]==1) return 0; //Obstacle at start
-        arr[0][0]=1;
-        for (int j=1;j<m;j++){
-            if (arr[0][j]==1) arr[0][j]=0;
-            else arr[0][j]=arr[0][j-1];
+    vector<vector<long long>>dp;
+    long long find(vector<vector<int>>& nums,int i,int j,int &n,int &m){
+        if(i>n-1 || j>m-1) return 0; 
+        if(i==n-1 && j==m-1){
+            return 1;
         }
-        for (int i=1; i<n;i++) {
-            if (arr[i][0]==1) arr[i][0] = 0;
-            else arr[i][0]=arr[i-1][0];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if (arr[i][j] == 1) {
-                    arr[i][j] = 0;
-                    continue;
-                }
-                int a=arr[i-1][j];
-                int b=arr[i][j-1];
-                arr[i][j]=a+b;
-            }
-        }
-        return arr[n-1][m-1];
-
+        if(dp[i][j]!=-1) return dp[i][j];
+        long long a=0;
+        long long b=0;
+        if(i+1<=n-1 && nums[i+1][j]!=1) a=find(nums,i+1,j,n,m);
+        if(j+1<=m-1 && nums[i][j+1]!=1) b=find(nums,i,j+1,n,m);
+        return dp[i][j]=a+b;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int n=Grid.size();
+        int m=Grid[0].size();
+        if(Grid[0][0] == 1 || Grid[n-1][m-1] == 1) return 0;
+        dp.resize(n+1,vector<long long>(m+1,-1));
+        long long a=find(Grid,0,0,n,m);
+        return (int)a;
     }
 };
+
