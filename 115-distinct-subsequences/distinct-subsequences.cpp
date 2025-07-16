167c50d4962b92fa6b033ae882148ba4dd21cb54
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-17 01:28:48 +0530

    Time: 163 ms (11.47%) | Memory: 383.8 MB (11.30%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int find(string &s1,string &s2,int &n1,int &n2,int i,int j){
-        if(j>=n2) return 1;
-        if(i>=n1) return 0;
+    int find(string &s1,string &s2,int i,int j){
+        if(j>=s2.length()) return 1;
+        if(i>=s1.length()) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-            return dp[i][j]=find(s1,s2,n1,n2,i+1,j+1)+find(s1,s2,n1,n2,i+1,j);
+            return dp[i][j]=find(s1,s2,i+1,j+1)+find(s1,s2,i+1,j);
         }
-        else return dp[i][j]=find(s1,s2,n1,n2,i+1,j);
+        return dp[i][j]=find(s1,s2,i+1,j);
     }
     int numDistinct(string s, string t) {
-        int n1=s.length();
-        int n2=t.length();
-        dp.resize(n1+1,vector<int>(n2+1,-1));
-        return find(s,t,n1,n2,0,0);
+        dp.resize(1001,vector<int>(1001,-1));
+        return find(s,t,0,0);
     }
 };
