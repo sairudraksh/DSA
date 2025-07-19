3fc8d34a3cf16b1ba04219c44d8627dffaf0cb35
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-20 00:19:03 +0530

    Time: 3 ms (73.41%) | Memory: 96.1 MB (85.73%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,44 +1,40 @@
-//[1,2,1,1,2,1,2]// like for example we want for this so %2=0 will be when all are even 
-//2  %2=0 will also be when all are odd
-// when %2=1 means they are alternative 
-// so here remainder of all are same in 3 cases means these can be all possible types
-
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
         int n=nums.size();
-        int maxcount=0;
         int count=0;
+        int maxcount=-1;
         for(int i=0;i<n;i++){
             if(nums[i]%2==0) count++;
         }
-        maxcount=max(count,maxcount);
+        maxcount=max(maxcount,count);
         count=0;
         for(int i=0;i<n;i++){
             if(nums[i]%2!=0) count++;
         }
-        maxcount=max(maxcount,count);
+        maxcount=max(count,maxcount);
         count=0;
-        int preval;
+
+        int prevans=0;
         for(int i=0;i<n;i++){
             if(i==0){
-                preval=nums[0]%2;
+                prevans=nums[i]%2;
                 count++;
             }
-            else if(preval==0){
+            else if(prevans==0){
                 if(nums[i]%2!=0){
+                    prevans=nums[i]%2;
                     count++;
-                    preval=nums[i]%2;
                 }
             }
-            else if(preval==1){
-                if(nums[i]%2==0){
+            else if(prevans==1){
+                if(nums[i]%2!=1){
+                    prevans=nums[i]%2;
                     count++;
-                    preval=nums[i]%2;
                 }
             }
         }
-        maxcount=max(maxcount,count);
+        maxcount=max(count,maxcount);
         return maxcount;
     }
 };
