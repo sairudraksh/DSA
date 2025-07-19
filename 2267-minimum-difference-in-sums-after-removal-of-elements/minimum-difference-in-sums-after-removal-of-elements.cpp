cac2a27f3d169b1ecafc548c22a923b6b640e696
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-19 23:03:11 +0530

    Time: 131 ms (84.05%) | Memory: 154.9 MB (44.05%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.cpp
@@ -3,40 +3,42 @@ public:
     long long minimumDifference(vector<int>& nums) {
         int N=nums.size();
         int n=N/3;
-        int k=n;
-        vector<long long>prefix(N,0);
-        vector<long long>suffix(N,0);
 
+        vector<long long>leftside(N,0);
+        vector<long long>rightside(N,0);
 
-        priority_queue<int>leftheap;
-        priority_queue<int,vector<int>, greater<int>> rightheap;
+        priority_queue<int>maxheap;
+        priority_queue<int,vector<int>,greater<int>> minHeap;
 
 
         long long leftsum=0;
         long long rightsum=0;
-        for(int i=0;i<2*n;i++){
+        for(int i=0;i<=2*n-1;i++){
             leftsum+=nums[i];
-            leftheap.push(nums[i]);
-            if(leftheap.size()>k){
-                leftsum-=leftheap.top();
-                leftheap.pop();
+            maxheap.push(nums[i]);
+            if(maxheap.size()>n){
+                int x=maxheap.top();
+                leftsum-=x;
+                maxheap.pop();
             }
-            prefix[i]=leftsum;
+            leftside[i]=leftsum;
         }
+
         for(int i=N-1;i>=n;i--){
             rightsum+=nums[i];
-            rightheap.push(nums[i]);
-            if(rightheap.size()>k){
-                rightsum-=rightheap.top();
-                rightheap.pop();
+            minHeap.push(nums[i]);
+            if(minHeap.size()>n){
+                int x=minHeap.top();
+                rightsum-=x;
+                minHeap.pop();
             }
-            suffix[i]=rightsum;
+            rightside[i]=rightsum;
         }
-        long long ans=LLONG_MAX;
+        long long minans=LLONG_MAX;
         for(int i=n-1;i<=2*n-1;i++){
-            long long diff=prefix[i]-suffix[i+1];
-            ans=min(ans,diff);
+            long long diff=leftside[i]-rightside[i+1];
+            minans=min(diff,minans);
         }
-        return ans;
+        return minans;
     }
 };
