82fdd1bcbddfe63ecf76a191c5624f805dc67f33
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-20 23:43:09 +0530

    Time: 77 ms (55.61%) | Memory: 98.3 MB (44.32%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
+        int N=meetings.size();
         sort(meetings.begin(),meetings.end());
-        vector<long long>lastavailable(n,0);
-        vector<long long>roomsusedcount(n,0);
+        vector<long long>ongoing(n,0);
+        vector<long long>countofroom(n,0);
+
         for(int i=0;i<meetings.size();i++){
             long long start=meetings[i][0];
             long long end=meetings[i][1];
             long long d=end-start;
-            long long minend=LLONG_MAX;
-            int minidx=0;
-            bool flag=true;
-            for(int room=0;room<n;room++){
-                if(lastavailable[room]<=start){
-                    lastavailable[room]=end;
-                    roomsusedcount[room]++;
-                    flag=false;
+            long long minans=LLONG_MAX;
+            long long minidx=-1;
+            bool flagg=true;
+            for(int rooms=0;rooms<n;rooms++){
+                if(ongoing[rooms]<=start){
+                    ongoing[rooms]=end;
+                    countofroom[rooms]++;
+                    flagg=false;
                     break;
                 }
-                if(lastavailable[room]<minend){
-                    minend=lastavailable[room];
-                    minidx=room;
+                if(ongoing[rooms]<minans){
+                    minans=ongoing[rooms];
+                    minidx=rooms;
                 }
             }
-            if(flag){
-                lastavailable[minidx]+=d;
-                roomsusedcount[minidx]++;
+            if(flagg==true){
+                ongoing[minidx]+=d;
+                countofroom[minidx]++;
             }
         }
         int ans=0;
-        for(int i=1;i<n;i++){
-            if(roomsusedcount[i]>roomsusedcount[ans]){
-                ans=i;
+        int ansidx=0;
+        for(int i=0;i<n;i++){
+            if(countofroom[i]>ans){
+                ans=countofroom[i];
+                ansidx=i;
             }
         }
-        return ans;
+        return ansidx;
     }
 };
