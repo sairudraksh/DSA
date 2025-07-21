23bf3b8356c8733fa28fec5b0f69a11518b54fe0
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-22 02:23:46 +0530

    Time: 75 ms (56.48%) | Memory: 100.4 MB (23.88%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
-    vector<vector<long long>>jobs;
-    vector<long long>dp;
-    int find(long long i){
-        if(i>=jobs.size()) return 0;
+    vector<vector<int>>ans;
+    vector<int>dp;
+    int find(int i){
+        if(i>=ans.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        long long x=jobs.size();
-        long long l=i+1;
-        long long r=jobs.size()-1;
+        int l=i+1;
+        // for(int k=i+1;k<ans.size();k++){// this will give tle
+        //     if(ans[k][0]>=ans[i][1]){
+        //         x=k;
+        //         break;
+        //     }
+        // }
+        int r=ans.size()-1;
+        int x=ans.size();
         while(l<=r){
-            long long mid=(r+l)/2;
-            if(jobs[mid][0]>=jobs[i][1]){
-                x=mid;
+            int mid=(l+r)/2;
+            if(ans[mid][0]>=ans[i][1]){
                 r=mid-1;
+                x=mid;
             }
-            else l=mid+1;
+            else{
+                l=mid+1;
+            }
         }
-        long long a=jobs[i][2]+find(x);
-        long long b=find(i+1);
-        return dp[i]=max(a,b);
+        return dp[i]=max(ans[i][2]+find(x),find(i+1));
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        jobs.clear();
-        for(long long i=0;i<startTime.size();i++){
-            jobs.push_back({(long long)startTime[i],(long long)endTime[i],(long long)profit[i]});
+        ans.clear();
+        int n=startTime.size();
+        for(int i=0;i<startTime.size();i++){
+            ans.push_back({startTime[i],endTime[i],profit[i]});
         }
-        dp.resize(startTime.size()+1,-1);
-        sort(jobs.begin(),jobs.end());
+        sort(ans.begin(),ans.end());
+        dp.resize(n+1,-1);
         return find(0);
     }
 };
