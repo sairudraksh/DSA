e5992f0e7862e741a625e8b9a28b96572d7feaa3
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-22 10:06:00 +0530

    Time: 13 ms (37.64%) | Memory: 150.5 MB (18.38%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -3,29 +3,36 @@ public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
         int m=startTime.size();
         vector<int>freearray;
+
         freearray.push_back(startTime[0]);
         for(int i=1;i<m;i++){
             freearray.push_back(startTime[i]-endTime[i-1]);
         }
-        freearray.push_back(eventTime-endTime[endTime.size()-1]);
+        freearray.push_back(eventTime-endTime[m-1]);
+
         int n=freearray.size();
-        vector<int>maxright(n);
-        vector<int>maxleft(n);
-        maxright[n-1]=0;
-        int result=0;
-        for(int i=n-2;i>=0;i--){
-            maxright[i]=max(maxright[i+1],freearray[i+1]);
-        }
+        vector<int>maxright(n,0);
+        vector<int>maxleft(n,0);
+
+        
         maxleft[0]=0;
         for(int i=1;i<n;i++){
             maxleft[i]=max(maxleft[i-1],freearray[i-1]);
         }
+
+        maxright[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            maxright[i]=max(maxright[i+1],freearray[i+1]);
+        }
+        int result=INT_MIN;
         for(int i=1;i<n;i++){
             int d=endTime[i-1]-startTime[i-1];
-            if(d<=maxleft[i-1] || d<=maxright[i]){
-                result=max(result,freearray[i-1]+freearray[i]+d);
+            if(d<=maxright[i]  || d<=maxleft[i-1]){
+                result=max(result,freearray[i]+freearray[i-1]+d);
             }
-            result=max(result,freearray[i-1]+freearray[i]);
+            else{
+                result=max(result,freearray[i]+freearray[i-1]);
+            }
         }
         return result;
     }
