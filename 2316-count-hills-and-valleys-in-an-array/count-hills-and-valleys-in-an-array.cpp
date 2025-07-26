0abd36c8c044929b04fd742247789db88ddeaddb
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-07-27 02:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (19.85%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -8,25 +8,21 @@ public:
             if(nums[i]==nums[i-1]) continue;
             int l=i-1;
             int r=i+1;
-            bool flagg1=false;
             while(nums[l]==nums[i]){
                 l--;
                 if(l<0){
-                    flagg1=true;
                     break;
                 }
             }
-            if(flagg1==true) continue;
+            if(l<0) continue;
 
-            bool flagg2=false;
             while(nums[r]==nums[i]){
                 r++;
                 if(r>=n){
-                    flagg2=true;
                     break;
                 }
             }
-            if(flagg2==true) continue;
+            if(r>=n) continue;
 
             if(((nums[l]<nums[i] && nums[i]>nums[r])   ||  (nums[l]>nums[i] && nums[r]>nums[i]))) count++;
         }
