5fd71dcaec81b3f719128d3a1ea7e678a16e5595
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-01 02:09:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (62.59%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>v;
+
+        for(int i=1;i<=n;i++){
+            vector<int>v1(i);
+            v.push_back(v1);
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i) v[i][j]=1;
+                else v[i][j]=v[i-1][j]+v[i-1][j-1];
+            }
+        }
+        return v;
+    }
+};
