9a640c28a8abf034d8e954c1ed32124371d050c5
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-01 18:59:24 +0530

    Time: 167 ms (24.14%) | Memory: 111.3 MB (15.46%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool find(vector<int>& nums,int i,int &sum,int ans){
-        if(i>=nums.size()) return false;
-        if(dp[i][ans]!=-1) return dp[i][ans];
-        int a=sum-ans;
-        if(a==ans) return true;
-        return dp[i][ans]=find(nums,i+1,sum,ans+nums[i]) | find(nums,i+1,sum,ans);
+    bool find(vector<int>&nums,int i,int target){
+        if(target==0) return true;
+        if(target<0) return false;
+        else if(i>=nums.size()) return false;
+        if(dp[i][target]!=-1) return dp[i][target];
+        bool take=find(nums,i+1,target-nums[i]);
+        bool notTake=find(nums,i+1,target);
+
+        return dp[i][target]=(take || notTake);
     }
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        dp.resize(n+1,vector<int>(sum+1,-1));
-        return find(nums,0,sum,0);
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        dp.resize(n+1,vector<int>(target+1,-1));
+        return find(nums,0,target);
     }
 };
