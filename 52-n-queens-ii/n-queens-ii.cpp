cd34c36023ee746368745513ebff3985fb55e6cf
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-03 11:02:25 +0530

    Time: 4 ms (26.68%) | Memory: 11.3 MB (10.60%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    vector<vector<char>>grid;
+
+    bool wecanput(int row,int col,int n){
+        for(int i=row-1;i>=0;i--){
+            if(grid[i][col]=='Q') return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(grid[i][j]=='Q') return false;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(grid[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void find(int row,int n){
+        if(row==n){
+            vector<string>v;
+            for(int i=0;i<n;i++){
+                string s="";
+                for(int j=0;j<n;j++){
+                    s+=grid[i][j];
+                }
+                v.push_back(s);
+            }
+            ans.push_back(v);
+            return;
+        }
+        for(int j=0;j<n;j++){
+            if(wecanput(row,j,n)){
+                grid[row][j]='Q';
+                find(row+1,n);
+                grid[row][j]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        grid.clear();
+        ans.clear();
+        grid.resize(n,vector<char>(n,'.'));
+        find(0,n);
+        return ans.size();
+    }
+};
