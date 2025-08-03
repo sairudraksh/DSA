4deed7943af38bf001e73690c5f9ad9e0af5023f
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-03 15:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (38.03%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void find(vector<int>&nums,int target,int i,vector<int>&v){
+    void find(vector<int>& nums,int target,int i,vector<int>&v){
         if(target==0){
             ans.push_back(v);
             return;
         }
-        if(i==nums.size()) return;
+        if(i>=nums.size()) return;
+
         if(nums[i]<=target){
             v.push_back(nums[i]);
             find(nums,target-nums[i],i+1,v);
             v.pop_back();
-            
         }
+
         int j=i+1;
         while(j<nums.size() && nums[j]==nums[j-1]) j++;
         find(nums,target,j,v);
@@ -20,9 +21,8 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<int>v;
-        ans.clear();
-        int i=0;
-        find(nums,target,i,v);
+
+        find(nums,target,0,v);
         return ans;
     }
 };
