d07d78096d455410632a785f92dd67b04bd989dc
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-03 12:16:22 +0530

    Time: 230 ms (79.36%) | Memory: 8.8 MB (26.96%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool issafe(vector<vector<char>>& board,int row,int col,int num){
-        //row check
-        for(int i=0;i<9;i++){
-            if(board[row][i]-'0'==num) return false;
+    bool isvalid(vector<vector<char>>& grid,int row,int col,int num){
+        for(int j=0;j<9;j++){
+            if(grid[row][j]-'0'==num) return false;
         }
-        // col check
+
         for(int i=0;i<9;i++){
-            if(board[i][col]-'0'==num) return false;
+            if(grid[i][col]-'0'==num) return false;
         }
+
         int x=(row/3)*3;
         int y=(col/3)*3;
         for(int i=x;i<x+3;i++){
             for(int j=y;j<y+3;j++){
-                if(board[i][j]-'0'==num) return false;
+                if(grid[i][j]-'0'==num) return false;
             }
         }
         return true;
@@ -21,11 +21,11 @@ public:
     bool find(vector<vector<char>>& board,int row,int col){
         if(row==9) return true;
         if(col==9) return find(board,row+1,0);
-        if(board[row][col]!='.'){
-            return find(board,row,col+1);
-        }
+
+        if(board[row][col]!='.') return find(board,row,col+1);
+
         for(int i=1;i<=9;i++){
-            if(issafe(board,row,col,i)){
+            if(isvalid(board,row,col,i)){
                 board[row][col]='0'+i;
                 bool a=find(board,row,col+1);
                 if(a==true) return true;
@@ -35,9 +35,7 @@ public:
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        int row=0;
-        int col=0;
-        find(board,row,col);
-
+        int n=board.size();
+        find(board,0,0);
     }
 };
