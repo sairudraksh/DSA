55007c513f07784e39aa43d91ba9a1ca0ebd9863
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-05 13:46:56 +0530

    Time: 95 ms (16.15%) | Memory: 128.3 MB (42.25%) - LeetSync

--- 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,48 +1,43 @@
 class Solution {
 public:
-    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        int n=fruits.size();
+    int maxTotalFruits(vector<vector<int>>& nums, int Pos, int k) {
+        int n=nums.size();
+
         vector<int>prefixSum(n);
         vector<int>indices(n);
 
         for(int i=0;i<n;i++){
-            indices[i]=fruits[i][0];
-
-            prefixSum[i]=fruits[i][1]+(i>0?prefixSum[i-1]:0);
+            indices[i]=nums[i][0];
+            prefixSum[i]=nums[i][1]+(i>0?prefixSum[i-1]:0);
         }
-        int maxFruits=0;
-
+        int maximum=0;
         for(int d=0;d<=k/2;d++){
-            int remain=k-2*d;
-
-            int i=startPos-d;
-            int j=startPos+remain;
+            int i=Pos-d;
+            int j=Pos+(k-(2*d));
 
             int left=lower_bound(indices.begin(),indices.end(),i)-indices.begin();
 
             int right=upper_bound(indices.begin(),indices.end(),j)-indices.begin()-1;
-            
 
             if(left<=right){
-                int total=prefixSum[right]-(left>0?prefixSum[left-1]:0);
-                maxFruits=max(maxFruits,total);
+                int ans=prefixSum[right]-(left>0?prefixSum[left-1]:0);
+                maximum=max(maximum,ans);
             }
 
+            i=Pos+d;
+            j=Pos-(k-(2*d));
 
-            i=startPos-remain;
-            j=startPos+d;
+            left=lower_bound(indices.begin(),indices.end(),j)-indices.begin();
 
-            left=lower_bound(indices.begin(),indices.end(),i)-indices.begin();
-
-            right=upper_bound(indices.begin(),indices.end(),j)-indices.begin()-1;
+            right=upper_bound(indices.begin(),indices.end(),i)-indices.begin()-1;
             
-
             if(left<=right){
-                int total=prefixSum[right]-(left>0?prefixSum[left-1]:0);
-                maxFruits=max(maxFruits,total);
+                int ans=prefixSum[right]-(left>0?prefixSum[left-1]:0);
+                maximum=max(maximum,ans);
             }
 
         }
-        return maxFruits;
+
+        return maximum;
     }
 };
