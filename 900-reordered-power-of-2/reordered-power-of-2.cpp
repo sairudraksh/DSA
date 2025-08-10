0d4fc96063eaa6da3248a5cfc132bde49e33d418
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-10 15:16:42 +0530

    Time: 598 ms (5.01%) | Memory: 9.2 MB (34.22%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     bool isPower(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        return n>0 && (n & (n-1))==0;
     }
-    
-    bool find(const string& s, string& ans, vector<bool>& used) {
-        if (ans.length() == s.length()) {
-            int t = stoi(ans);
-            return isPower(t);
+    bool find(string &s,string &ans,vector<int>&visited){
+        if(ans.length()==s.length()){
+            int t=stoi(ans);
+            if(isPower(t)){
+                return true;
+            }
+            return false;
         }
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (used[i]) continue;
-            if (ans.length() == 0 && s[i] == '0') continue; // Skip leading zero
-            
-            used[i] = true;
-            ans.push_back(s[i]);
-            
-            if (find(s, ans, used)) return true;
-            
+
+        for(int i=0;i<s.length();i++){
+            if(visited[i]) continue;
+            if(ans.length()==0 && s[i]=='0') continue;
+
+            char org=s[i];
+            ans+=s[i];
+            visited[i]=true;
+
+            bool a=find(s,ans,visited);
+
+            if(a==true) return true;
+
             ans.pop_back();
-            used[i] = false;
+            visited[i]=false;
         }
         return false;
     }
-    
     bool reorderedPowerOf2(int n) {
-        string s = to_string(n);
-        vector<bool> used(s.length(), false);
-        string ans = "";
-        return find(s, ans, used);
+        string s=to_string(n);
+        string ans="";
+        vector<int>visited(s.length(),false);
+        return find(s,ans,visited);
     }
 };
