44128b9c4eb82415304f2d507f3a3d3e6e5904ad
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-08-11 17:20:32 +0530

    Time: 20 ms (42.42%) | Memory: 95.8 MB (52.83%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int find(int num){
+        int count=0;
+        if(num%3==0) return num/3;
+        else{
+            int k=3;
+            while(k<num-1){
+                k=k+3;
+                count++;
+            }
+            k=k-3;
+
+        return count+(num-k)/2;
+        }
+    }
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>map;
+        for(int i=0;i<n;i++){
+            map[nums[i]]++;
+        }
+        int count=0;
+        for(auto &x:map){
+            if(x.second==1) return -1;
+            else{
+                count+=find(x.second);
+            }
+        }
+        return count;
+    }
+};
