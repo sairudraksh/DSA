e22850832dbe841480fe03fde5d3dc3908fb7fc1
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-06 11:26:19 +0530

    Time: 2042 ms (13.61%) | Memory: 94.6 MB (92.77%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -2,69 +2,71 @@ class Solution {
 public:
     int n;
     int m;
-    int find(int i,int j,vector<vector<int>>& grid,int direction,int turn){
+    int find(vector<vector<int>>& grid,int i,int j,int direction,bool hasTurned){
         if(i>=n || j>=m || i<0 || j<0 || grid[i][j]==1) return 0;
-        int ans1=-1;
-        int ans2=-1;
-        if(direction==1){// bottom right
-            if(grid[i-1][j-1]==grid[i][j]) return 0;
-            ans1=1+find(i+1,j+1,grid,direction,turn);
-            if(turn) ans2=1+find(i+1,j-1,grid,2,0);
+
+
+        int maximum=0;
+        if(direction==1){
+            int prev=grid[i-1][j-1];
+            if(prev==1 && grid[i][j]!=2) return 0;
+            if(grid[i][j]==prev) return 0;
+            int a=1+find(grid,i+1,j+1,direction,hasTurned);
+            int b=0;
+            if(!hasTurned) b=1+find(grid,i+1,j-1,2,true);
+            maximum=max(a,b);
         }
 
-        else if(direction==2){//bottom left
-            if(grid[i-1][j+1]==grid[i][j]) return 0;
-            ans1=1+find(i+1,j-1,grid,direction,turn);
-            if(turn) ans2=1+find(i-1,j-1,grid,4,0);
+        if(direction==2){
+            int prev=grid[i-1][j+1];
+            if(prev==1 && grid[i][j]!=2) return 0;
+            if(grid[i][j]==prev) return 0;
+            int a=1+find(grid,i+1,j-1,direction,hasTurned);
+            int b=0;
+            if(!hasTurned) b=1+find(grid,i-1,j-1,3,true);
+            maximum=max(a,b);
         }
 
-        else if(direction==3){//top right
-            if(grid[i+1][j-1]==grid[i][j]) return 0;
-            ans1=1+find(i-1,j+1,grid,direction,turn);
-            if(turn) ans2=1+find(i+1,j+1,grid,1,0);
+        if(direction==3){
+            int prev=grid[i+1][j+1];
+            if(prev==1 && grid[i][j]!=2) return 0;
+            if(grid[i][j]==prev) return 0;
+            int a=1+find(grid,i-1,j-1,direction,hasTurned);
+            int b=0;
+            if(!hasTurned) b=1+find(grid,i-1,j+1,4,true);
+            maximum=max(a,b);
         }
 
-        else if(direction==4){//top left
-            if(grid[i+1][j+1]==grid[i][j]) return 0;
-            ans1=1+find(i-1,j-1,grid,direction,turn);
-            if(turn) ans2=1+find(i-1,j+1,grid,3,0);
+        if(direction==4){
+            int prev=grid[i+1][j-1];
+            if(prev==1 && grid[i][j]!=2) return 0;
+            if(grid[i][j]==prev) return 0;
+            int a=1+find(grid,i-1,j+1,direction,hasTurned);
+            int b=0;
+            if(!hasTurned) b=1+find(grid,i+1,j+1,1,true);
+            maximum=max(a,b);
         }
-
-        return max(ans1,ans2);
-
+        return maximum;
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         n=grid.size();
         m=grid[0].size();
-        if(n==1 || m==1){
-            if(grid[0][0]==1) return 1;
-            return 0;
-        } 
-        int maximum=1;
-        int onecount=0;
+
+        int maximum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    onecount++;
-                    if(i+1<n && j+1<m && grid[i+1][j+1]==2){
-                        maximum=max(maximum,1+find(i+1,j+1,grid,1,1));
-                    }
+                
+                    maximum=max(maximum,1+find(grid,i+1,j+1,1,false));
 
-                    if(i+1<n && j-1>=0 && grid[i+1][j-1]==2){
-                        maximum=max(maximum,1+find(i+1,j-1,grid,2,1));
-                    }
+                    maximum=max(maximum,1+find(grid,i+1,j-1,2,false));
 
-                    if(i-1>=0 && j+1<m && grid[i-1][j+1]==2){
-                        maximum=max(maximum,1+find(i-1,j+1,grid,3,1));
-                    }
+                    maximum=max(maximum,1+find(grid,i-1,j-1,3,false));
 
-                    if(i-1>=0 && j-1>=0 && grid[i-1][j-1]==2){
-                        maximum=max(maximum,1+find(i-1,j-1,grid,4,1));
-                    }
-                }
+                    maximum=max(maximum,1+find(grid,i-1,j+1,4,false));
+                }       
             }
         }
-        if(onecount==0) return 0;
         return maximum;
     }
 };
