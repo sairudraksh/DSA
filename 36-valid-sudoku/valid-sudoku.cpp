9c5238fb6fae05dab98c33a2ad5d1d35a6690015
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-06 10:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.5 MB (90.84%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& grid,int row,int col,char num){
-        for(int j=0;j<9;j++){
-            if(j!=col && grid[row][j]==num) return false;
+    bool isValid(vector<vector<char>>&board,int r,int c,char &num){
+        for(int i=0;i<9;i++){
+            if(i==c) continue;
+            if(board[r][i]==num) return false;
         }
 
         for(int i=0;i<9;i++){
-            if(i!=row && grid[i][col]==num) return false;
+            if(i==r) continue;
+            if(board[i][c]==num) return false;
         }
 
-        int x=(row/3)*3;
-        int y=(col/3)*3;
+        int x=(r/3)*3;
+        int y=(c/3)*3;
+
         for(int i=x;i<x+3;i++){
             for(int j=y;j<y+3;j++){
-                if((i != row || j != col) && grid[i][j] == num) return false;
+                if(i==r && j==c) continue;
+                if(board[i][j]==num) return false;
             }
         }
         return true;
     }
-    bool find(vector<vector<char>>&grid,int row,int col){
-        if(row>=9) return true;
-        if(col>=9) return find(grid,row+1,0);
-
-        if(grid[row][col]=='.'){
-            return find(grid,row,col+1);
-        }
-        if(!isValid(grid,row,col,grid[row][col])) return false;
-        return find(grid,row,col+1);
-    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        return find(board,0,0);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                if(!isValid(board,i,j,board[i][j])){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
