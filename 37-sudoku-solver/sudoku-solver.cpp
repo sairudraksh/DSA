823bccd9a543eab1bb4d7dd04d9ff4b5eed1527e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-06 09:57:12 +0530

    Time: 228 ms (70.51%) | Memory: 8.8 MB (30.43%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    bool isvalid(vector<vector<char>>& grid,int row,int col,int num){
-        for(int j=0;j<9;j++){
-            if(grid[row][j]-'0'==num) return false;
+    bool isSafe(vector<vector<char>>& grid,int r,int c,int num){
+        for(int i=0;i<9;i++){
+            if(grid[r][i]-'0'==num) return false;
         }
 
         for(int i=0;i<9;i++){
-            if(grid[i][col]-'0'==num) return false;
+            if(grid[i][c]-'0'==num) return false;
         }
 
-        int x=(row/3)*3;
-        int y=(col/3)*3;
+
+        int x=(r/3)*3;
+        int y=(c/3)*3;
+
         for(int i=x;i<x+3;i++){
             for(int j=y;j<y+3;j++){
                 if(grid[i][j]-'0'==num) return false;
@@ -18,24 +20,26 @@ public:
         }
         return true;
     }
-    bool find(vector<vector<char>>& board,int row,int col){
-        if(row==9) return true;
-        if(col==9) return find(board,row+1,0);
+    int f(vector<vector<char>>& board,int r,int c){
+        if(r>=9) return true;
+        if(c>=9) return f(board,r+1,0);
 
-        if(board[row][col]!='.') return find(board,row,col+1);
+        if(board[r][c]!='.') return f(board,r,c+1);
+        
+        for(int j=1;j<=9;j++){
+            if(isSafe(board,r,c,j)){
+                board[r][c]='0'+j;
 
-        for(int i=1;i<=9;i++){
-            if(isvalid(board,row,col,i)){
-                board[row][col]='0'+i;
-                bool a=find(board,row,col+1);
+                bool a=f(board,r,c+1);
+
                 if(a==true) return true;
-                board[row][col]='.';
+
+                board[r][c]='.'; 
             }
         }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        int n=board.size();
-        find(board,0,0);
+        f(board,0,0);
     }
 };
