8b00232ce75e2673b91836e300d07bbe555d06be
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-14 16:55:54 +0530

    Time: 1260 ms (8.33%) | Memory: 461.4 MB (8.33%) - LeetSync

---
+++ 4027-number-of-stable-subsequences/number-of-stable-subsequences.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    const int M=1e9+7;
+   vector<vector<vector<int>>> dp; 
+    int find(vector<int>& nums,int i,int prev,int Sprev){
+        if(i>=nums.size()){
+            if(prev==-1 && Sprev==-1) return 0;
+            return 1;
+        }
+        int p1=2;
+        int p2=2;
+        if(prev!=-1){
+            if(nums[prev]%2==0) p1=0;
+            else p1=1;
+        }
+        if(Sprev!=-1){
+            if(nums[Sprev]%2==0) p2=0;
+            else p2=1;
+        }
+        if(dp[i][p1][p2]!=-1) return dp[i][p1][p2];
+        if(prev==-1 || Sprev==-1){
+            return dp[i][p1][p2]=(find(nums,i+1,i,prev)+find(nums,i+1,prev,Sprev))%M;
+        }
+
+        else{
+            if(nums[prev]%2==0 && nums[Sprev]%2==0 && nums[i]%2==0){
+                return dp[i][p1][p2]=find(nums,i+1,prev,Sprev)%M;
+            }
+            else if(nums[prev]%2!=0 && nums[Sprev]%2!=0 && nums[i]%2!=0){
+                return dp[i][p1][p2]=find(nums,i+1,prev,Sprev)%M;
+            }
+            else{
+                return dp[i][p1][p2]=(find(nums,i+1,i,prev)+find(nums,i+1,prev,Sprev))%M;
+            }
+        }
+    }
+    int countStableSubsequences(vector<int>& nums) {
+        int n=nums.size();
+        dp.assign(n, vector<vector<int>>(3, vector<int>(3, -1)));
+        return find(nums,0,-1,-1)%M;
+    }
+};
