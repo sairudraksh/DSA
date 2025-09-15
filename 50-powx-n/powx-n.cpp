779254ae7a35d2152b99d596c50c16997e604fd7
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-15 13:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.87%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
+    double find(double x,long n){
+        if(n==0) return 1;
+
+        if(n<0) return find(1/x,-n);
+
+        if(n%2==0) return find(x*x,n/2);
+        else return x*find(x*x,(n-1)/2);
+    }
     double myPow(double x, int n) {
-        return pow(x,n);
+        return find(x,(long)n);
     }
 };
