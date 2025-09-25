f06a0b0fa0173225de95f7a08aafb7a0eb52e379
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-09-25 08:15:55 +0530

    Time: 3 ms (13.39%) | Memory: 13.4 MB (42.12%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>>dp;
-    int find(vector<vector<int>>& nums,int i,int j){
-        if(i==nums.size()-1) return nums[i][j];
+    int find(vector<vector<int>>&triangle,int i,int j){
+        if(i>=n || j>=triangle[i].size()) return INT_MAX;
         if(dp[i][j]!=INT_MIN) return dp[i][j];
-        return dp[i][j]=min(nums[i][j]+find(nums,i+1,j),nums[i][j]+find(nums,i+1,j+1));
+        int a=find(triangle,i+1,j);
+        int b=find(triangle,i+1,j+1);
         
+        if(a==INT_MAX && b==INT_MAX) return dp[i][j]=triangle[i][j];
+        else return dp[i][j]=triangle[i][j]+min(find(triangle,i+1,j),find(triangle,i+1,j+1));
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        dp.resize(n+1,vector<int>(n+1,INT_MIN));
+        n=triangle.size();
+        int m=triangle[n-1].size();
+        dp.resize(n+1,vector<int>(m+1,INT_MIN));
         return find(triangle,0,0);
     }
 };
