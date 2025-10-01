83455f693c29e3d29fd9e32a2b5d7e90c560d061
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-01 09:23:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (0.09%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numWaterBottles(int nums, int x) {
+        unordered_map<int,int>mp;
+        int count=0;
+        for(int i=0;i<nums;i++){
+            mp[1]++;
+            count++;
+        }
+
+
+        while(mp[1]>=x){
+            mp[1]-=x;
+            count+=1;
+            mp[1]++;
+        }
+        return count;
+    }
+};
