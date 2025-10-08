5b4331c997543e48e52a0d487714a401add58048
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-08 09:38:22 +0530

    Time: 51 ms (6.52%) | Memory: 27.7 MB (6.18%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
+    int n;
+    int m;
     vector<vector<int>>dp;
-    int find(string &s1,string &s2,int &n1,int &n2,int i,int j){
-        if(i>=n1 || j>=n2) return 0;
+    int find(string &s1,string &s2,int i,int j){
+        if(i>=n || j>=m) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-            return dp[i][j]=1+find(s1,s2,n1,n2,i+1,j+1);
+            return dp[i][j]=1+find(s1,s2,i+1,j+1);
         }
-        return dp[i][j]=max(find(s1,s2,n1,n2,i+1,j),find(s1,s2,n1,n2,i,j+1));
+        else{
+            return dp[i][j]=max(find(s1,s2,i+1,j),find(s1,s2,i,j+1));
+        }
     }
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m=text2.length();
-        int i=0;
-        int j=0;
+    int longestCommonSubsequence(string s1, string s2) {
+        n=s1.length();
+        m=s2.length();
         dp.resize(n+1,vector<int>(m+1,-1));
-        return find(text1,text2,n,m,i,j);
+        return find(s1,s2,0,0);
     }
 };
