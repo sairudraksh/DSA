5630fccaa1012bacff3cb795dc7ceead50a8d19f
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-08 10:39:04 +0530

    Time: 33 ms (58.18%) | Memory: 51 MB (16.30%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int find(string &s1,string &s2,int i,int j,int &n1,int &n2){
-        if(i>=n1 || j>=n2) return 0;
+    vector<vector<long long>>dp;
+    long long find(string &s,int i,int j){
+        if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]){
-            return dp[i][j]=1+find(s1,s2,i+1,j+1,n1,n2);
+        if(s[i]==s[j]){
+            return dp[i][j]=find(s,i+1,j-1);
         }
-        return dp[i][j]=max(find(s1,s2,i+1,j,n1,n2),find(s1,s2,i,j+1,n1,n2));
+        return dp[i][j]=1+min(find(s,i+1,j),find(s,i,j-1));
     }
-    int minInsertions(string s1) {
-        int n1=s1.length();
-        string s2=s1;
-        reverse(s2.begin(),s2.end());
-        int n2=s2.length();
-        dp.resize(n1+1,vector<int>(n2+1,-1));
-        int a=find(s1,s2,0,0,n1,n2);
-        return s1.length()-a;
+    int minInsertions(string s) {
+        int n=s.length();
+        dp.resize(n+1,vector<long long>(n+1,-1));
+        return (int)find(s,0,n-1);
     }
 };
