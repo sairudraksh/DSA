1e2d8e098dc74fb53f8f7b330221330bbcd75155
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-13 13:18:33 +0530

    Time: 4 ms (17.04%) | Memory: 11.4 MB (20.79%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     map<vector<int>,int>map;
     vector<vector<int>>finalans;
+    int n;
     void find(vector<int>&nums,int i,vector<int>&ans){
-        if(map.find(ans)==map.end()){
-            finalans.push_back(ans);
-            map[ans]++;
+        if(i>=n){
+            if(map.find(ans)==map.end()){
+                finalans.push_back(ans);
+                map[ans]++;
+            }
+            return;
         }
-        if(i>=nums.size()) return;
-        find(nums,i+1,ans);
+
         ans.push_back(nums[i]);
         find(nums,i+1,ans);
         ans.pop_back();
+        find(nums,i+1,ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<int>arr;
-        find(nums,0,arr);
+        n=nums.size();
+        vector<int>v;
+        find(nums,0,v);
         return finalans;
     }
 };
