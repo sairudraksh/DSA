907771d079e79a30f8a2c5efd73c4a713e82feda
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-14 12:17:08 +0530

    Time: 40 ms (57.22%) | Memory: 75.2 MB (39.28%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int totalFruit(vector<int>& nums) {
         int n=nums.size();
-        unordered_map<int,int>map;
-
-        int len=0;
 
         int i=0;
         int j=0;
 
+        unordered_map<int,int>map;
+        int maximum=0;
         while(j<n){
             map[nums[j]]++;
 
@@ -18,10 +17,9 @@ public:
                 i++;
             }
 
-            len=max(len,(j-i)+1);
+            maximum=max(maximum,(j-i)+1);
             j++;
         }
-
-        return len;
+        return maximum;
     }
 };
