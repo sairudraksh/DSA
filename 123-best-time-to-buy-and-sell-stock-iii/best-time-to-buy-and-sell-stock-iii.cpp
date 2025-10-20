b886b7ed7613bf9ddf921a976a1310f8a85ae2de
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-21 00:47:07 +0530

    Time: 50 ms (67.83%) | Memory: 90.1 MB (61.75%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-
-    int find(vector<int>&prices,int i,int k,bool flagg){
-        if(i>=prices.size()) return 0;
-        if(dp[i][k][flagg]!=-1) return dp[i][k][flagg];
-        if(flagg==false){
-            int a=0;
-            int b=0;
-            if(dp[i][k][flagg]!=-1) return dp[i][k][flagg];
-            if(k>0){
-                a=-prices[i]+find(prices,i+1,k-1,true);
-            }
-            b=find(prices,i+1,k,false);
-            return dp[i][k][flagg]=max(a,b);
+    int dp[100005][2][2];
+    int find(vector<int>&nums,int i,bool holding,int count){
+        int n=nums.size();
+        if(count==2) return 0;
+        if(i>=n) return 0;
+        if(dp[i][holding][count]!=-1) return dp[i][holding][count];
+        if(holding){
+            return dp[i][holding][count]=max(nums[i]+find(nums,i+1,false,count+1),find(nums,i+1,true,count));
         }
         else{
-            return dp[i][k][flagg]=max(prices[i]+find(prices,i+1,k,false),find(prices,i+1,k,true));
+            return dp[i][holding][count]=max(find(nums,i+1,true,count)-nums[i],find(nums,i+1,false,count));
         }
     }
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        dp = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(3, vector<int>(2, -1)));
-        return find(prices,0,2,false);
+    int maxProfit(vector<int>& nums) {
+        int n=nums.size();
+        memset(dp, -1, sizeof(dp)); 
+        return find(nums,0,false,0);
     }
 };
