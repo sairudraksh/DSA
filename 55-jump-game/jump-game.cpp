5abd8e341152ae34cd4e2d50a9283b796cd76d22
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-26 21:39:00 +0530

    Time: 2 ms (30.31%) | Memory: 52.2 MB (88.50%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    vector<int>dp;
-    int find(vector<int>&nums,int i){
-        int n=nums.size();
-        if(i>=n) return false;
-        else if(i==n-1) return true;
-        else if(nums[i]==0) return false;
-        if(dp[i]!=-1) return dp[i];
-
-        for(int k=1;k<=nums[i];k++){
-            bool a=find(nums,i+k);
-            if(a==true) return dp[i]=true;
-        }
-        return dp[i]=false;
-    }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        dp.resize(n+1,-1);
-        return find(nums,0);
+        int far=0;
+
+        for(int i=0;i<n;i++){
+            if(i>far) return false;
+            far=max(far,i+nums[i]);
+        }
+        return true;
     }
 };
