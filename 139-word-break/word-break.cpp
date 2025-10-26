a0f83fa2347cedf4654ea957100a9fc26ce43c3a
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-26 18:38:45 +0530

    Time: 43 ms (5.11%) | Memory: 38 MB (5.11%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -2,29 +2,28 @@ class Solution {
 public:
     unordered_map<string,int>map;
     vector<vector<int>>dp;
-    bool find(string &str,vector<string>& wordDict,int i,int j){
-        if(j>=str.length()) return false;
+    bool find(string s,int i,int j){
+        if(j>=s.length()) return false;
         if(dp[i][j]!=-1) return dp[i][j];
-        string s1=str.substr(i,j-i+1);
+        string s1=s.substr(i,j-i+1);
         bool a=false;
         bool b=false;
+
         if(map.find(s1)!=map.end()){
-            if(j==str.length()-1) return true;
-            a=find(str,wordDict,j+1,j+1);
+            if(j==s.length()-1) return dp[i][j]=true;
+            a=find(s,j+1,j+1);
         }
-        b=find(str,wordDict,i,j+1);
+        b=find(s,i,j+1);
+
         if(a==true || b==true) return dp[i][j]=true;
-        else return dp[i][j]=false;
+        return dp[i][j]=false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n=wordDict.size();
-        int m=s.length();
-        dp.resize(m+1,vector<int>(m+1,-1));
+        int n=s.length();
+        dp.resize(n+1,vector<int>(n+1,-1));
         for(int i=0;i<wordDict.size();i++){
             map[wordDict[i]]++;
         }
-        string str="";
-        return find(s,wordDict,0,0);
-
+        return find(s,0,0);
     }
 };
