d8e76420c07a91a6835df774e8447cc13c671f2c
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-26 18:56:50 +0530

    Time: 1524 ms (5.04%) | Memory: 373.2 MB (5.00%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<int>dp;
-    int find(int amount,vector<int>coins){
-        if(amount==0) return 0;
-        if(dp[amount]!=-2) return dp[amount];
-        int result=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            if(amount-coins[i]<0) continue;
-            result=min(result,find(amount-coins[i],coins));
-        }
-        if(result==INT_MAX) return dp[amount]=INT_MAX;
-        return dp[amount]=1+result;
+    int find(vector<int>nums,int target){
+        if(target==0) return 0;
+        if(dp[target]!=-1) return dp[target];
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(target>=nums[i]){
+                target-=nums[i];
+                int a=find(nums,target);
+                target+=nums[i];
+                if(a!=INT_MAX){
+                    ans=min(ans,1+a);
+                }
+            }
+        } 
+        return dp[target]=ans;
     }
     int coinChange(vector<int>& coins, int amount) {
-        dp.clear();
-        dp.resize(amount+1,-2);
-        int a=find(amount,coins);
+        dp.resize(amount+1,-1);
+        int a=find(coins,amount);
         if(a==INT_MAX) return -1;
-        else return a;
+        return a;
     }
 };
