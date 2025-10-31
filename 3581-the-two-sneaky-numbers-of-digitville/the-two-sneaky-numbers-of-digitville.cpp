cd41a52a98197873a3bb8e32e7cb1b797eb10cfa
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-31 12:29:38 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (86.35%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                v.push_back(nums[i]);
+            }
+        }
+        return v;
+    }
+};
