faed004918179d3e0c93e885097e492a95ce5765
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-31 16:58:33 +0530

    Time: 4 ms (81.41%) | Memory: 14.7 MB (15.05%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
+    int n1;
+    int n2;
     vector<vector<int>>dp;
-    int find(string &word1,string &word2,int i,int j){
-        if(i==-1 && j==-1) return 0;
-        else if(i==-1 && j!=-1){
-            return j+1;
+    int find(string &s1,string &s2,int i,int j){
+        if(i>=n1 && j>=n2) return 0;
+        else if(i>=n1){
+            return n2-j;
         }
-        else if(i!=-1 && j==-1){
-            return i+1;
+        else if(j>=n2){
+            return n1-i;
         }
         if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]){
-            return dp[i][j]=find(word1,word2,i-1,j-1);
+        if(s1[i]==s2[j]){
+            return dp[i][j]=find(s1,s2,i+1,j+1);
         }
-        return dp[i][j]=1+min({find(word1,word2,i-1,j-1),find(word1,word2,i-1,j),find(word1,word2,i,j-1)});
+        else{
+            int a=find(s1,s2,i,j+1);
+            int b=find(s1,s2,i+1,j);
+            int c=find(s1,s2,i+1,j+1);
+            return dp[i][j]=1+min({a,b,c});
+        }
         
     }
     int minDistance(string word1, string word2) {
-        dp.resize(word1.size()+1,vector<int>(word2.size()+1,-1));
-        return find(word1,word2,word1.length()-1,word2.length()-1);
+        n1=word1.length();
+        n2=word2.length();
+        dp.resize(n1+1,vector<int>(n2+1,-1));
+        return find(word1,word2,0,0);
     }
 };
