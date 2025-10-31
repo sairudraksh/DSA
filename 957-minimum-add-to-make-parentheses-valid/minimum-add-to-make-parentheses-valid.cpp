3d2f23db47c0d3ad0380469608efebf5e2681127
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-10-31 19:41:34 +0530

    Time: 1 ms (9.61%) | Memory: 8.5 MB (30.28%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -3,15 +3,18 @@ public:
     int minAddToMakeValid(string s) {
         int opening=0;
         int closing=0;
+        int n=s.length();
         int count=0;
         for(int i=0;i<s.length();i++){
             if(s[i]=='(') opening++;
             else closing++;
+
             if(closing>opening){
                 count++;
                 opening++;
             }
+            if(i==n-1) count+=abs(opening-closing);
         }
-        return count+(opening-closing);
+        return count;
     }
 };
