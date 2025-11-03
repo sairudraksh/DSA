28b067469e06938f8cb55310ba0e0deaabdb87a7
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-03 22:39:16 +0530

    Time: 3 ms (32.77%) | Memory: 16.3 MB (6.54%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,20 +3,28 @@ public:
     int n;
     int m;
     vector<vector<int>>dp;
-    int find(vector<vector<int>>& matrix,int i ,int j){
-        if(j>=m || j<0) return INT_MAX;
-        if(i==n-1) return matrix[i][j];
+    int find(vector<vector<int>>&grid,int i,int j){
+        if(i>=n || i<0 || j>=m || j<0) return INT_MAX;
+        if(i==n-1) return grid[i][j];
         if(dp[i][j]!=-100000) return dp[i][j];
-        return dp[i][j]=matrix[i][j]+min({find(matrix,i+1,j+1),find(matrix,i+1,j-1),find(matrix,i+1,j)});
+        int a=INT_MAX;
+        int b=INT_MAX;
+        int c=INT_MAX;
+        a=find(grid,i+1,j);
+        b=find(grid,i+1,j+1);
+        c=find(grid,i+1,j-1);
+
+        if(a==INT_MAX && b==INT_MAX && c==INT_MAX) return dp[i][j]=INT_MAX;
+        return dp[i][j]=grid[i][j]+min({a,b,c});
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         n=matrix.size();
         m=matrix[0].size();
-        int minimum=INT_MAX;
+        int ans=INT_MAX;
         dp.resize(n+1,vector<int>(m+1,-100000));
         for(int i=0;i<m;i++){
-            minimum=min(minimum,find(matrix,0,i));
+            ans=min(ans,find(matrix,0,i));
         }
-        return minimum;
+        return ans;
     }
 };
