a116666dfc7aebf426304500f4e620cc62774f06
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-03 22:12:45 +0530

    Time: 3 ms (10.86%) | Memory: 11.8 MB (53.70%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    vector<vector<long long>>dp;
-    long long find(vector<vector<int>>& nums,int i,int j,int &n,int &m){
-        if(i>n-1 || j>m-1) return 0; 
-        if(i==n-1 && j==m-1){
-            return 1;
-        }
+    int n;
+    int m;
+    vector<vector<int>>dp;
+    int find(vector<vector<int>>&grid,int i,int j){
+        if(i>=n || j>=m) return 0;
+        if(grid[i][j]==1) return 0;
+        if(i==n-1 && j==m-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        long long a=0;
-        long long b=0;
-        if(i+1<=n-1 && nums[i+1][j]!=1) a=find(nums,i+1,j,n,m);
-        if(j+1<=m-1 && nums[i][j+1]!=1) b=find(nums,i,j+1,n,m);
-        return dp[i][j]=a+b;
+        return dp[i][j]=find(grid,i+1,j)+find(grid,i,j+1);
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
-        int n=Grid.size();
-        int m=Grid[0].size();
-        if(Grid[0][0] == 1 || Grid[n-1][m-1] == 1) return 0;
-        dp.resize(n+1,vector<long long>(m+1,-1));
-        long long a=find(Grid,0,0,n,m);
-        return (int)a;
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return find(grid,0,0);
     }
 };
-
