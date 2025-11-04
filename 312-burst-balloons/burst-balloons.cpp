131afbdc7d63c4161f23096f0fe5df4b97e7e1a3
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-04 12:04:13 +0530

    Time: 208 ms (18.64%) | Memory: 13.9 MB (57.60%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>>dp;
-    int find(vector<int>& nums,int i,int j){
-        if(i>j) return 0;
+    int find(vector<int>&nums,int i,int j){
+        if(i==j) return nums[i]*nums[i-1]*nums[i+1];
         if(dp[i][j]!=-1) return dp[i][j];
-
-        int maximum=INT_MIN;
+        int maximum=0;
         for(int k=i;k<=j;k++){
-            maximum=max(maximum,(nums[i-1]*nums[k]*nums[j+1])+find(nums,i,k-1)+find(nums,k+1,j));
+            maximum=max(maximum,(nums[k]*nums[i-1]*nums[j+1])+find(nums,i,k-1)+find(nums,k+1,j));
         }
-
         return dp[i][j]=maximum;
     }
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        int n=nums.size();
+        n=nums.size();
         dp.resize(n+1,vector<int>(n+1,-1));
         return find(nums,1,n-2);
     }
