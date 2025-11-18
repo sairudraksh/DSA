075e093c2a2f53990805b807041fa90acfef3a89
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-18 11:18:23 +0530

    Time: 275 ms (57.29%) | Memory: 10.7 MB (58.60%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -3,31 +3,26 @@ public:
     int n;
     int m;
     vector<vector<int>>visited;
-    bool find(vector<vector<char>>& board, string &word,int i,int j,int idx){
-        if(i>=n || j>=m || i<0 || j<0 || visited[i][j]==1) return false;
-
+    bool find(vector<vector<char>>& board,string &word,string &s,int i,int j,int idx){
+        if(s==word) return true;
+        if(i>=n || j>=m || i<0 || j<0) return false;
+        if(visited[i][j]!=-1) return false;
         if(board[i][j]!=word[idx]) return false;
-        if(idx==word.size()-1) return true;
-        else if(idx>=word.size()) return false;
         visited[i][j]=1;
-
-        bool a=find(board,word,i+1,j,idx+1);
-        bool b=find(board,word,i,j+1,idx+1);
-        bool c=find(board,word,i-1,j,idx+1);
-        bool d=find(board,word,i,j-1,idx+1);
-
-        visited[i][j]=0;
-        if(a || b || c || d) return true;
-        else return false;
+        s+=board[i][j];
+        bool a={find(board,word,s,i+1,j,idx+1)||find(board,word,s,i,j+1,idx+1)||find(board,word,s,i,j-1,idx+1)||find(board,word,s,i-1,j,idx+1)};
+        s.pop_back();
+        visited[i][j]=-1;
+        return a;
     }
     bool exist(vector<vector<char>>& board, string word) {
         n=board.size();
         m=board[0].size();
-        visited.resize(n,vector<int>(m+1,-1));
-        bool a=false;
+        visited.resize(n+1,vector<int>(m+1,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                bool a=find(board,word,i,j,0);
+                string s="";
+                bool a=find(board,word,s,i,j,0);
                 if(a==true) return true;
             }
         }
