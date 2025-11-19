0a243e7c4a00f15f0ea9ac9ea3a73c5d3b9c8533
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-19 11:06:07 +0530

    Time: 155 ms (6.51%) | Memory: 55.4 MB (5.28%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
+    int n1;
+    int n2;
     vector<vector<int>>dp;
-    bool find(string &s,string &p,int i,int j){
-        if(i==-1 && j==-1) return true;
-        else if(j<0 && i>=0) return false;
-        else if(i<0 && j>=0){
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*') return false;
-            }
-            return true; 
-        } 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?'){// cannot skip here as it should be continous
-            return dp[i][j]=find(s,p,i-1,j-1);
+    bool find(string &s1,string &s2,int i,int j){
+        if(i>=n1){
+            while(j<n2 && s2[j]=='*') j++;
         }
-        else if(p[j]=='*'){
-            return dp[i][j]=find(s,p,i-1,j) | find(s,p,i,j-1);// slip * as it can match empty also
+        if(j>=n2){
+            while(i<n2 && s2[j]=='*') i++;
+        }
+        if(i>=n1 && j>=n2) return true;
+        if(i>=n1 || j>=n2) return false;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]=='*' || s2[j]=='*'){
+            return dp[i][j]=find(s1,s2,i+1,j) ||find(s1,s2,i+1,j+1) ||find(s1,s2,i,j+1);
+        }
+        else if(s1[i]==s2[j] || s1[i]=='?' || s2[j]=='?'){
+            return dp[i][j]=find(s1,s2,i+1,j+1);
         }
         return dp[i][j]=false;
     }
-    bool isMatch(string s, string p) {
-        int n=s.length();
-        int m=p.length();
-        dp.resize(n+1,vector<int>(m+1,-1));
-        return find(s,p,n-1,m-1);
+    bool isMatch(string s1, string s2) {
+        n1=s1.length();
+        n2=s2.length();
+        dp.resize(n1+1,vector<int>(n2+1,-1));
+        return find(s1,s2,0,0);
     }
 };
