bb8eb868f22b5fd45cf4a7797f6906e51de3c03e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-20 09:39:51 +0530

    Time: 709 ms (5.13%) | Memory: 296.7 MB (6.10%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>>dp;
-    int find(vector<int>&nums,int i,int prevans){
+    int find(vector<int>&nums,int i,int prev){
         if(i>=nums.size()) return 0;
-        if(dp[i][prevans+1]!=-1) return dp[i][prevans+1];
-        if(prevans==-1 || nums[i]>nums[prevans]){
-            return dp[i][prevans+1]=max(1+find(nums,i+1,i),find(nums,i+1,prevans));
+        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+        if(prev==-1 || nums[i]>nums[prev]){
+            return dp[i][prev+1]=max(1+find(nums,i+1,i),find(nums,i+1,prev));
         }
-        else return dp[i][prevans+1]=find(nums,i+1,prevans);
+        else{
+            return dp[i][prev+1]=find(nums,i+1,prev);
+        }
     }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
+        n=nums.size();
         dp.resize(n+1,vector<int>(n+1,-1));
         return find(nums,0,-1);
     }
