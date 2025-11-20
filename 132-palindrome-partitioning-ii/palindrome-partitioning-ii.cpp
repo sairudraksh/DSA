b3ca740067ccc6556e683415a650a38deae3a475
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-20 16:36:08 +0530

    Time: 1838 ms (5.10%) | Memory: 32.5 MB (18.02%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    int maxans=INT_MAX;
+    int n;
     vector<int>dp;
-    bool ispalindrome(string &ans){
-        if(ans.length()==1) return true;
+    bool isPalindrome(string &s){
         int i=0;
-        int j=ans.length()-1;
+        int j=s.length()-1;
         while(i<=j){
-            if(ans[i]!=ans[j]) return false;
+            if(s[i]!=s[j]) return false;
             i++;
             j--;
         }
         return true;
     }
-    int find(string &s,int &n,int i){
-        if(i>=n){
-            return 0;
-        }
+    int find(string &s,int i){
+        if(i>=n) return 0;
+        string ans="";
+
+        int a=INT_MAX;
         if(dp[i]!=-1) return dp[i];
-        string ans1="";
-        int result=INT_MAX;
         for(int k=i;k<n;k++){
-            ans1+=s[k];
-            if(ispalindrome(ans1)==true){
-                int a=1+find(s,n,k+1);
-                result=min(a,result);
+            ans+=s[k];
+            if(isPalindrome(ans)){
+                int b=1+find(s,k+1);
+                a=min(a,b);
             }
         }
-        return dp[i]=result;
-        
+        return dp[i]=a;
     }
     int minCut(string s) {
-        int n=s.length();
+        n=s.length();
+        if(n==1) return 0;
         dp.resize(n+1,-1);
-        int count=0;
-        return find(s,n,0)-1;
+        int a=find(s,0);
+        return a-1;
     }
 };
