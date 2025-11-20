b2ba7d35fd9de8eeb8a68f33ef3589135963e744
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-20 13:45:41 +0530

    Time: 20 ms (9.70%) | Memory: 31.5 MB (6.33%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -3,24 +3,24 @@ public:
     int n;
     int m;
     vector<vector<int>>dp;
-    int find(vector<vector<int>>& nums,int i,int j){
-        if(i>=n || j>=m || nums[i][j]==0) return 0;
+    int find(vector<vector<int>>& matrix,int i,int j){
+        if(i>=n || j>=m) return 0;
+        if(matrix[i][j]==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=1+min({find(nums,i+1,j+1),find(nums,i+1,j),find(nums,i,j+1)});
+        return dp[i][j]=1+min({find(matrix,i+1,j),find(matrix,i+1,j+1),find(matrix,i,j+1)});
     }
-    int countSquares(vector<vector<int>>& nums) {
-        n=nums.size();
-        m=nums[0].size();
+    int countSquares(vector<vector<int>>& matrix) {
+        n=matrix.size();
+        m=matrix[0].size();
+        int count=0;
         dp.resize(n+1,vector<int>(m+1,-1));
-
-        int sum=0;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                sum+=find(nums,i,j);
+                int a=0;
+                if(matrix[i][j]==1) a=find(matrix,i,j);
+                count+=a;
             }
         }
-
-        return sum;
+        return count;
     }
 };
