650206c4790eb6682c4f6ee232f86976565b4b96
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-29 15:18:33 +0530

    Time: 48 ms (58.17%) | Memory: 33 MB (19.59%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>>result;
+    void find(vector<int>&nums,int &target,int i,int j){
+        while(i<j){
+            if(nums[i]+nums[j]>target) j--;
+            else if(nums[i]+nums[j]<target) i++;
+            else{
+                while(i<j && nums[i]==nums[i+1]) i++;
+                while(j>i && nums[j]==nums[j-1]) j--;
+                result.push_back({-target,nums[i],nums[j]});
+                i++;
+                j--;
+            }
+        }
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        n=nums.size();
+        if(n<3) return {};
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            int target=-nums[i];
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+
+            find(nums,target,i+1,n-1);
+        }
+        return result;
+    }
+};
