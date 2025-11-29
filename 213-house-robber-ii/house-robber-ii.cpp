f2d181f18cf978cf0a45fc5d8fcb3d9f9a3006c9
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-29 13:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (7.39%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>>dp;
-    int find(int i,vector<int>&nums,bool flagg ,int &count,int &n){
+    int find(vector<int>&nums,int i,bool taken){
         if(i>=n) return 0;
-        if(i==n-1 && flagg==true) return nums[i];
-        if(i==n-1 && flagg==false) return 0;
-        if(dp[i][flagg]!=-1) return dp[i][flagg];
-        if(count==0){
-            count++;
-            return dp[i][flagg]=max(nums[i]+find(i+2,nums,false,count,n),find(i+1,nums,true,count,n));
+        if(i==n-1){
+            if(taken) return 0;
+            return nums[i];
         }
-        return dp[i][flagg]=max(nums[i]+find(i+2,nums,flagg,count,n),find(i+1,nums,flagg,count,n));
+        if(dp[i][taken]!=-1) return dp[i][taken];
+        if(i==0){
+            return dp[i][taken]=max(nums[i]+find(nums,i+2,true),find(nums,i+1,false));
+        }
+        return dp[i][taken]=max(nums[i]+find(nums,i+2,taken),find(nums,i+1,taken));
     }
     int rob(vector<int>& nums) {
-        dp.resize(1001,vector<int>(1001,-1));
-        bool flagg=true;
-        int count=0;
-        int i=0;
-        int n=nums.size();
-        return find(i,nums,flagg,count,n);
+        n=nums.size();
+        dp.resize(n+1,vector<int>(2,-1));
+        return find(nums,0,false);        
     }
 };
