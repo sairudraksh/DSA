3de6a766e1af06a8a6b9e9ee44b5a849965baa12
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-29 13:59:20 +0530

    Time: 59 ms (33.98%) | Memory: 14.8 MB (81.08%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -7,7 +7,7 @@ public:
         if(i1<0 || j1<0 || i1>=n || j1>=m || i2<0 || j2<0 || i2>=n || j2>=m) return 0;
         if(dp[i1][j1][j2]!=-1) return dp[i1][j1][j2];
         if(j1==j2){
-            return dp[i1][j1][j2]=grid[i1][j1]+max({find(grid,i1+1,j1+1,i2+1,j2),find(grid,i1+1,j1+1,i2+1,j2+1),find(grid,i1+1,j1+1,i2+1,j2-1),find(grid,i1+1,j1,i2+1,j2),find(grid,i1+1,j1,i2+1,j2+1),find(grid,i1+1,j1,i2+1,j2-1),find(grid,i1+1,j1-1,i2+1,j2),find(grid,i1+1,j1-1,i2+1,j2+1),find(grid,i1+1,j1-1,i2+1,j2-1)});
+            return dp[i1][j1][j2]=grid[i1][j1]+max({find(grid,i1+1,j1+1,i2+1,j2),find(grid,i1+1,j1+1,i2+1,j2+1),find(grid,i1+1,j1+1,i2+1,j2-1),find(grid,i1+1,j1,i2+1,j2),find(grid,i1+1,j1,i2+1,j2+1),find(grid,i1+1,j1,i2+1,j2-1),find(grid,i1+1,j1-1,i2+1,j2),find(grid,i1+1,j1-1,i2+1,j2+1),find(grid,i1+1,j1-1,i2+1,j2-1)});// we tried all combos
         }
         else {
             return dp[i1][j1][j2]=grid[i1][j1]+grid[i2][j2]+max({find(grid,i1+1,j1+1,i2+1,j2),find(grid,i1+1,j1+1,i2+1,j2+1),find(grid,i1+1,j1+1,i2+1,j2-1),find(grid,i1+1,j1,i2+1,j2),find(grid,i1+1,j1,i2+1,j2+1),find(grid,i1+1,j1,i2+1,j2-1),find(grid,i1+1,j1-1,i2+1,j2),find(grid,i1+1,j1-1,i2+1,j2+1),find(grid,i1+1,j1-1,i2+1,j2-1)});
