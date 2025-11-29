127bb36fbfd4b50273f6e414272d9c4da5f35a98
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-29 10:48:05 +0530

    Time: 2613 ms (5.02%) | Memory: 550 MB (5.35%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int n;
     vector<vector<vector<int>>> dp;
-    vector<int> find(vector<int>&nums,int prev,int i){
-        if(i>=n) return {};
+    vector<int> find(vector<int>&nums,int i,int prev){
+        if(i>=nums.size()) return {};
         if(dp[i][prev+1].size()>0) return dp[i][prev+1];
-        vector<int>notTake=find(nums,prev,i+1);
+        vector<int>notTake=find(nums,i+1,prev);
 
         vector<int>take;
 
         if(prev==-1 || nums[i]%nums[prev]==0){
-            take=find(nums,i,i+1);
+            take=find(nums,i+1,i);
             take.insert(take.begin(),nums[i]);
         }
         if(take.size()>notTake.size()) return dp[i][prev+1]=take;
-        else return dp[i][prev+1]=notTake;
+        return dp[i][prev+1]=notTake; 
     }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         n=nums.size();
-        dp.assign(n, vector<vector<int>>(n + 1));  
-        return find(nums,-1,0);
+        sort(nums.begin(),nums.end());
+         dp.assign(n, vector<vector<int>>(n + 1)); 
+        return find(nums,0,-1);    
     }
 };
