7c0c1108a12efd0c6e7f07b76c7818fa59761072
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-30 11:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.89%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -3,16 +3,15 @@ public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size();
         double prefix=1;
-        double suffix=1;
-        int ans=INT_MIN;
+        double suffix=1;;
+        int maximum=INT_MIN;
         for(int i=0;i<n;i++){
             if(prefix==0) prefix=1;
             if(suffix==0) suffix=1;
             prefix=prefix*nums[i];
             suffix=suffix*nums[n-i-1];
-            ans = max(ans,max((int)prefix,(int)suffix));
-
+            maximum=max(maximum,max((int)prefix,(int)suffix));
         }
-        return ans;
+        return maximum;
     }
 };
