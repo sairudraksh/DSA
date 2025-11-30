0a7767e841ef6b4a7ac2ad4460342ccb24cbe302
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-11-30 15:54:03 +0530

    Time: 6 ms (72.66%) | Memory: 23.3 MB (79.04%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -13,7 +13,7 @@ public:
             }
             else{
                 if(length!=0){
-                    length=LPS[length-1];// we an directly assgn and i will npt be incremented and it will check again 
+                    length=LPS[length-1];// we an directly assign and i will not be incremented and it will check again and dont do length-- and go to previous LPS and it is already LPS na dtry to aad it  there
                 }
                 else{
                     LPS[i]=0;
@@ -21,7 +21,7 @@ public:
                 }
             }
         }
-        return s.substr(0, LPS[n-1]);
+        return s.substr(0, LPS[n-1]);// till n-1 as LPS we want of full string not till particular index
 
     }
 };
