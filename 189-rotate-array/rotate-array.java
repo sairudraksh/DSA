ed9fdd801265b96638a839e6f789363aa196c13c
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-01 12:03:16 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (22.02%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static void rotate(int i,int j,int[] nums){
+    public void rotate2(int i,int j,int[] nums){
         while(i<=j){
             int temp=nums[i];
             nums[i]=nums[j];
@@ -11,8 +11,8 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
         k=k%n;
-        rotate(0,n-k-1,nums);
-        rotate(n-k,n-1,nums);
-        rotate(0,n-1,nums);
+        rotate2(0,n-k-1,nums);
+        rotate2(n-k,n-1,nums);
+        rotate2(0,n-1,nums);
     }
 }
