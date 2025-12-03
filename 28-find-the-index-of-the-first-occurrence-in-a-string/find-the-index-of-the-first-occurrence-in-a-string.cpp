a394428e8c4589cfe580d05ad687a428c89d66ad
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-03 10:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (29.66%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    bool isTrue(string s1,string s2,int i){
-        int m=s2.length();
-        for(int j=0;j<m;j++){
-            if(s1[i]!=s2[j]) return false;
-            i++;
-        }
-        return true;
-    }
-    int strStr(string nums1, string nums2) {
-        int n=nums1.length();
-        int m=nums2.length();
-        for(int i=0;i<n;i++){
-            if(nums1[i]==nums2[0]){
-                if(isTrue(nums1,nums2,i)){
-                    return i;
+    int strStr(string s1, string s2) {
+        int n1=s1.length();
+        int n2=s2.length();
+        for(int i=0;i<n1;i++){
+            if(s1[i]==s2[0]){
+                int k=i;
+                int j=0;
+                for(j=0;j<n2;j++){
+                    if(s1[k]!=s2[j]){
+                        break;
+                    }
+                    k++;
                 }
+                if(j==n2) return i;
             }
         }
         return -1;
