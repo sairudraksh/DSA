e6d7d8905240974e819a612b390275a986592df4
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-05 12:18:38 +0530

    Time: 142 ms (7.25%) | Memory: 183.8 MB (22.70%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,43 @@
+class LRUCache {
+public:
+    list<int>dll;
+    map<int,pair<list<int>::iterator,int>>mp;
+    int n;
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    void mostRecentlyUsed(int key){
+        dll.erase(mp[key].first);
+        dll.push_front(key);
+        mp[key].first=dll.begin();
+    }
+    int get(int key) {
+        if(mp.find(key)==mp.end()) return -1;
+        mostRecentlyUsed(key);
+        return mp[key].second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            mp[key].second=value;
+            mostRecentlyUsed(key);
+        }
+        else{
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};//dll.begin() will give the adress;
+            n--;
+        }
+        if(n<0){
+            mp.erase(dll.back());
+            dll.pop_back();
+            n++;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
