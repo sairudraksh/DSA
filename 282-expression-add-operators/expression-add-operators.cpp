bb165976b522d5523d3dd7532cd101787c1f7577
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-06 12:11:10 +0530

    Time: 384 ms (71.42%) | Memory: 92.8 MB (66.39%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string>ans;
+    void find(string &num,int target,int idx,long long curr,long long last,string path){
+        if(idx>=num.length()){
+            if(curr==target) ans.push_back(path);
+            return;
+        }
+
+        long long val=0;
+        string s="";
+        for(int i=idx;i<num.length();i++){
+            if(i>idx && num[idx]=='0') break;
+            val=val*10+(num[i]-'0');
+            s+=num[i];
+            if(idx==0){
+                find(num,target,i+1,val,val,s);
+            }
+            else{
+                find(num,target,i+1,curr+val,val,path+'+'+s);
+                find(num,target,i+1,curr-val,-val,path+'-'+s);
+                find(num,target,i+1,curr-last+(last*val),last*val,path+'*'+s);
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+        ans.clear();
+        find(num,target,0,0,0,"");
+        return ans;
+    }
+};
