941908adbb794242a2b74e4003ac3ba827435fe0
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-12 12:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (57.85%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int lo=0;
+        int hi=n-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(matrix[mid][m-1]>=target){
+                hi=mid-1;
+                ans=mid;
+            }
+            else lo=mid+1;
+        }
+        if(ans==-1) return false;
+        int row=ans;
+        int i=0;
+        int j=m-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(matrix[row][mid]==target) return true;
+            else if(matrix[row][mid]>target) j=mid-1;
+            else i=mid+1;
+        }
+        return false;
+    }
+}
