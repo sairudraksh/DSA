fe7046946cd0405fc6ac292bc49cc6c6b597be73
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-16 13:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (13.74%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int candy(vector<int>& nums) {
+    int candy(vector<int>& nums){
         int n=nums.size();
-
         vector<int>left(n,1);
         vector<int>right(n,1);
-
         for(int i=1;i<n;i++){
             if(nums[i]>nums[i-1]) left[i]=left[i]+left[i-1];
         }
-
         for(int i=n-2;i>=0;i--){
             if(nums[i]>nums[i+1]) right[i]=right[i]+right[i+1];
         }
-
         int sum=0;
         for(int i=0;i<n;i++){
-            sum+=max(left[i],right[i]);
+            int a=max(left[i],right[i]);
+            sum+=a;
         }
-
         return sum;
     }
 };
