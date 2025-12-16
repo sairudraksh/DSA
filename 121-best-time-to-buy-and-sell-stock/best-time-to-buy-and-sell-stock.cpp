b6e978d2e0d989a0289d068adb9080c07ac270e7
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2025-12-16 10:01:28 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (25.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>>dp;
-    int find(vector<int>&nums,int i,bool taken){
-        if(i>=nums.size()) return 0;
-        if(dp[i][taken]!=-1) return dp[i][taken];
-        if(taken){
-            return dp[i][taken]=max(find(nums,i+1,true),nums[i]);
+    int maxProfit(vector<int>& nums){
+        int n=nums.size();
+        int profit=0;
+        int buy=nums[0];
+        for(int i=1;i<n;i++){
+            if(buy>nums[i]){
+                buy=nums[i];
+            }
+            else{
+                profit=max(profit,(nums[i]-buy));
+            }
         }
-        else{
-            return dp[i][taken]=max(find(nums,i+1,taken),find(nums,i+1,true)-nums[i]);
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        n=prices.size();
-        dp.resize(n+1,vector<int>(2,-1));
-        return find(prices,0,false);
+        return profit;
     }
 };
