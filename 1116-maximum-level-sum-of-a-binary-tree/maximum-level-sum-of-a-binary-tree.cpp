310af3cf26c7b13c0336c479d9fbdf09473caec4
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-06 09:00:33 +0530

    Time: 26 ms (5.68%) | Memory: 107 MB (94.32%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -1,30 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void find(TreeNode* root,int &ans,int curr,int &target){
-        if(root==NULL) return;
+    int find(TreeNode* root,int curr,int target){
+        if(root==NULL) return 0;
         if(curr==target){
-            ans+=root->val;
-            return;
+            return root->val;
         }
-        find(root->left,ans,curr+1,target);
-        find(root->right,ans,curr+1,target);
+        return find(root->left,curr+1,target)+find(root->right,curr+1,target);
     }
-    int levels(TreeNode* root){
+    int levels( TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
     int maxLevelSum(TreeNode* root) {
+        if(root==NULL) return 0;
         int n=levels(root);
-        int maxans=INT_MIN;
-        int ansidx=0;
+        int maximum=INT_MIN;
+        int level=-1;
         for(int i=1;i<=n;i++){
-            int ans=0;
-            find(root,ans,1,i);
-            if(ans>maxans){
-                maxans=ans;
-                ansidx=i;
+            int a=find(root,1,i);
+            if(a>maximum){
+                maximum=a;
+                level=i;
             }
         }
-        return ansidx;
+        return level;
+
     }
 };
