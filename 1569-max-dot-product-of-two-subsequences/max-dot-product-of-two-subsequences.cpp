dcd098b350c96054d926d843156cd1241ec2f153
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-08 19:12:33 +0530

    Time: 34 ms (43.60%) | Memory: 19.1 MB (13.20%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
+    int n;
+    int m;
     vector<vector<int>>dp;
-    int find(vector<int>& nums1, vector<int>& nums2,int i,int j){
-        if(i>=nums1.size() || j>=nums2.size()) return INT_MIN;
+    int find(vector<int>&nums1,vector<int>&nums2,int i,int j){
+        if(i>=n || j>=m) return INT_MIN;
         if(dp[i][j]!=-1) return dp[i][j];
-        int ans=nums1[i]*nums2[j]+max(0,find(nums1,nums2,i+1,j+1));// any value returninng less than 0 includein int min means it is decreasing the solution so there is no point to take it
-
-        //not taking
-        int ans1=find(nums1,nums2,i+1,j);
-        int ans2=find(nums1,nums2,i,j+1);
-        return dp[i][j]=max({ans,ans1,ans2});
+        int ans=0;
+        int take=find(nums1,nums2,i+1,j+1);
+        ans+=nums1[i]*nums2[j];
+        int notTake=max(find(nums1,nums2,i+1,j),find(nums1,nums2,i,j+1));
+        if(take>0){
+            take+=ans;
+            return dp[i][j]=max(take,notTake);
+        }
+        return dp[i][j]=max({ans,take,notTake});
     }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
-        dp.resize(n1+1,vector<int>(n2+1,-1));
+        n=nums1.size();
+        m=nums2.size();
+        dp.resize(n+1,vector<int>(m+1,-1));
         return find(nums1,nums2,0,0);
     }
 };
