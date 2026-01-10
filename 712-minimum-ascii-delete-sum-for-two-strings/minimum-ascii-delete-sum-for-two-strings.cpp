40496e0a5dc0ab0020c270eccbb4513e163838f0
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-10 08:36:05 +0530

    Time: 39 ms (20.02%) | Memory: 18.4 MB (23.97%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    int n;
+    int m;
     vector<vector<int>>dp;
-    int find(string &s1,string &s2,int i,int j,int &n1,int &n2){
-        if(i>=n1 && j>=n2) return 0;
-        if(i>=n1){
+    int find(string &s1,string &s2,int i,int j){
+        if(i>=n && j>=m) return 0;
+        if(i>=n){
             int sum=0;
-            for(int k=j;k<n2;k++){
-                sum=sum+(int)s2[k];
+            while(j<m){
+                char ch=s2[j];
+                int a=int(ch);
+                sum+=a;
+                j++;
             }
             return sum;
         }
-        else if(j>=n2){
+        if(j>=m){
             int sum=0;
-            for(int k=i;k<n1;k++){
-                sum=sum+(int)s1[k];
+            while(i<n){
+                char ch=s1[i];
+                int a=int(ch);
+                sum+=a;
+                i++;
             }
             return sum;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i]==s2[j]){
-            return dp[i][j]=find(s1,s2,i+1,j+1,n1,n2);
+            return dp[i][j]=find(s1,s2,i+1,j+1);
         }
-        else return dp[i][j]=min((int)s1[i]+find(s1,s2,i+1,j,n1,n2),(int)s2[j]+find(s1,s2,i,j+1,n1,n2));
+        else{
+            return dp[i][j]=min(int(s1[i])+find(s1,s2,i+1,j),int(s2[j])+find(s1,s2,i,j+1));
+        }
     }
     int minimumDeleteSum(string s1, string s2) {
-        int n1=s1.length();
-        int n2=s2.length();
-        dp.resize(n1+1,vector<int>(n2+1,-1));
-        return find(s1,s2,0,0,n1,n2);
+        n=s1.length();
+        m=s2.length();
+        dp.resize(n+1,vector<int>(m+1,-1));
+        return find(s1,s2,0,0);
     }
 };
