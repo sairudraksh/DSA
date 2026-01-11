5051a4bcbacfe70643c5768b2fcba4b5573b2c00
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-11 10:01:58 +0530

    Time: 7 ms (58.75%) | Memory: 23.5 MB (5.12%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,63 +1,65 @@
 class Solution {
 public:
-    int n;
-    int m;
-    vector<int> findPSE(vector<int>&heights){
+    vector<int>findpse(vector<int>v){
+        int n=v.size();
         stack<int>st;
+        vector<int>ans(n,-1);
         st.push(0);
-        vector<int>ans(m,-1);
-        for(int i=1;i<m;i++){
-            while(st.size()>0 && heights[st.top()]>=heights[i]){
+        for(int i=1;i<n;i++){
+            while(st.size()>0 && v[st.top()]>=v[i]){
                 st.pop();
             }
-            if(st.size()>0) ans[i]=st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    vector<int> findNSE(vector<int>&heights){
-        stack<int>st;
-        st.push(m-1);
-        vector<int>ans(m,m);
-        for(int i=m-2;i>=0;i--){
-            while(st.size()>0 && heights[st.top()]>=heights[i]){
-                st.pop();
-            }
-            if(st.size()>0) ans[i]=st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-    int MAH(vector<int>&heights){
-        vector<int>NSE=findNSE(heights);
-        vector<int>PSE=findPSE(heights);
+            if(st.size()==0) ans[i]=-1;
+            else ans[i]=st.top();
 
-        int ans=0;
-        for(int i=0;i<m;i++){
-            int breadth=NSE[i]-PSE[i]-1;
-            ans=max(ans,breadth*heights[i]);
+            st.push(i);
         }
         return ans;
+    }
+    vector<int>findnse(vector<int>v){
+        int n=v.size();
+        stack<int>st;
+        vector<int>ans(n,n);
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && v[st.top()]>=v[i]){
+                st.pop();
+            }
+            if(st.size()==0) ans[i]=n;
+            else ans[i]=st.top();
+
+            st.push(i);
+        }
+        return ans;
+    }
+    int MAH(vector<int>&v){
+        vector<int>nse=findnse(v);
+        vector<int>pse=findpse(v);
+        int n=v.size();
+        int maximum=0;
+        for(int i=0;i<n;i++){
+            int length=v[i];
+            int breadth=nse[i]-pse[i]-1;
+            maximum=max(maximum,length*breadth);
+        }
+        return maximum;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        n=matrix.size();
-        m=matrix[0].size();
-
-        vector<int>heights(m,0);
-
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int>v;
         for(int i=0;i<m;i++){
-            if(matrix[0][i]=='1') heights[i]=1;
-            else heights[i]=0;
+            if(matrix[0][i]=='1') v.push_back(1);
+            else v.push_back(0);
         }
         int maximum=0;
-        maximum=max(maximum,MAH(heights));
-
+        maximum=max(maximum,MAH(v));
         for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1') heights[j]++;
-                else heights[j]=0; 
+                if(matrix[i][j]=='1') v[j]++;
+                else v[j]=0;
             }
-            maximum=max(maximum,MAH(heights));
+            maximum=max(maximum,MAH(v));
         }
         return maximum;
     }
