4a09a4aa60957ccfa09baeca7830f8c4cfb0ce08
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-21 15:36:21 +0530

    Time: 231 ms (71.77%) | Memory: 8.9 MB (30.19%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>& grid,int r,int c,int num){
+    int m;
+    int n;
+    bool isTrue(vector<vector<char>>&boards,int r,int c,int num){
         for(int i=0;i<9;i++){
-            if(grid[r][i]-'0'==num) return false;
+            if(boards[r][i]-'0'==num) return false;
         }
-
         for(int i=0;i<9;i++){
-            if(grid[i][c]-'0'==num) return false;
+            if(boards[i][c]-'0'==num) return false;
         }
-
 
         int x=(r/3)*3;
         int y=(c/3)*3;
 
         for(int i=x;i<x+3;i++){
             for(int j=y;j<y+3;j++){
-                if(grid[i][j]-'0'==num) return false;
+                if(boards[i][j]-'0'==num) return false;
             }
         }
         return true;
     }
-    int f(vector<vector<char>>& board,int r,int c){
-        if(r>=9) return true;
-        if(c>=9) return f(board,r+1,0);
-
+    int f(vector<vector<char>>&board,int r,int c){
+        if(r>=n) return true;
+        if(c>=m) return f(board,r+1,0);
         if(board[r][c]!='.') return f(board,r,c+1);
-        
-        for(int j=1;j<=9;j++){
-            if(isSafe(board,r,c,j)){
-                board[r][c]='0'+j;
+        for(int i=1;i<=9;i++){
+
+            if(isTrue(board,r,c,i)){
+                board[r][c]=i+'0';
 
                 bool a=f(board,r,c+1);
 
                 if(a==true) return true;
 
-                board[r][c]='.'; 
+                board[r][c]='.';
             }
         }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
+        n=board.size();
+        m=board[0].size();
         f(board,0,0);
     }
 };
