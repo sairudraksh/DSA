7fcd2d14cf27258be19fea76560fa41d2af1ade4
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-22 11:16:54 +0530

    Time: 139 ms (14.20%) | Memory: 34.9 MB (12.54%) - LeetSync

--- 87-scramble-string/scramble-string.cpp
+++ 87-scramble-string/scramble-string.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    unordered_map<string,bool>map;
-    bool solve(string s1,string s2){
+    unordered_map<string,int>map;
+    bool find(string s1,string s2){
+        int n=s1.length();
         if(s1==s2) return true;
         if(s1.length()!=s2.length()) return false;
-        string key=s1+"_"+s2;
-        if(map.find(key)!=map.end()) return map[key];
-        int n=s1.length();
-        bool result=false;
+        string s=s1+"_"+s2;
+        if(map.find(s)!=map.end()) return map[s];
+        bool found=false;
         for(int i=1;i<s1.length();i++){
-
-            bool swapped=solve(s1.substr(0, i), s2.substr(n-i, i)) && solve(s1.substr(i, n-i), s2.substr(0, n-i));
+            bool swapped=find(s1.substr(0,i),s2.substr(n-i,i)) && find(s1.substr(i,n-i),s2.substr(0,n-i));
 
             if(swapped){
-                result=true;
+                found=true;
                 break;
             }
 
-            bool not_swapped = solve(s1.substr(0, i), s2.substr(0, i)) && solve(s1.substr(i, n-i), s2.substr(i, n-i));
-            if(not_swapped){
-                result=true;
+            bool notSwapped=find(s1.substr(0,i),s2.substr(0,i)) && find(s1.substr(i,n-i),s2.substr(i,n-i));
+
+            if(notSwapped){
+                found=true;
                 break;
             }
         }
-        return map[key]=result;
+        return map[s]=found;
     }
-    bool isScramble(string s1, string s2) {
+    bool isScramble(string s1, string s2){
         map.clear();
-        return solve(s1,s2);
+        return find(s1,s2);
     }
 };
