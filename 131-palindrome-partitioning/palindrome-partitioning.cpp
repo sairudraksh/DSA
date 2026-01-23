96b7c17ffe3674b389c11e3e43dd2ed6f4ce100f
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-23 17:00:25 +0530

    Time: 74 ms (20.40%) | Memory: 99.9 MB (15.43%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -12,31 +12,41 @@ public:
         }
         return true;
     }
-    void find(string &s,vector<string>&v,string &str,int i){
-        if(i>=s.length()){
-            if(str=="") return;
+    void find(string &s,int i,string &str,vector<string>&v){
+        if(i>=n){
+            if(str.length()>0){
+                if(isPalindrome(str)){
+                    v.push_back(str);
+                    ans.push_back(v);
+                    v.pop_back();
+                }
+            }
+            return ;
+        }
+        if(str.length()==0){
+            str+=s[i];
+            find(s,i+1,str,v);
+            str.pop_back();
+        }
+        else{
             if(isPalindrome(str)){
                 v.push_back(str);
-                ans.push_back(v);
-                v.pop_back();
+                string t=str;
+                str="";
+                find(s,i,str,v);
+                if(v.size()>0) v.pop_back();
+                str=t;
             }
-            return;
+            str+=s[i];
+            find(s,i+1,str,v);
+            str.pop_back();
         }
-        str+=s[i];
-        if(isPalindrome(str)){
-            v.push_back(str);
-            string neww="";
-            find(s,v,neww,i+1);
-            v.pop_back();
-        }
-        find(s,v,str,i+1);
-        str.pop_back();
     }
     vector<vector<string>> partition(string s) {
         n=s.length();
+        string t="";
         vector<string>v;
-        string str="";
-        find(s,v,str,0);
+        find(s,0,t,v);
         return ans;
     }
 };
