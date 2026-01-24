bb296d9a1f24139c4177fcdf85cd0c424de4a546
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-24 23:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int n;
     vector<int>dp;
     int find(vector<int>&nums,int i){
         if(i>=nums.size()) return 0;
         if(dp[i]!=-1) return dp[i];
-        return dp[i]=max(nums[i]+find(nums,i+2),find(nums,i+1));
+        return dp[i]=max(find(nums,i+1),nums[i]+find(nums,i+2));
     }
     int rob(vector<int>& nums) {
-        n=nums.size();
+        int n=nums.size();
         dp.resize(n+1,-1);
         return find(nums,0);
     }
