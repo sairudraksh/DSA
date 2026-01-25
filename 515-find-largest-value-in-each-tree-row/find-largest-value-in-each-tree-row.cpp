77bf4d4ba3284d6c2c64c45ed9ddf8e17cec6792
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-25 21:28:51 +0530

    Time: 2 ms (14.96%) | Memory: 22.7 MB (86.77%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -1,27 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void find(TreeNode* root,int curr,int target,int &ans){
+    void find(TreeNode* root,int &ans,int curr,int target){
         if(root==NULL) return;
         if(curr==target){
             if(root->val>ans){
                 ans=root->val;
             }
+            return;
         }
-        find(root->left,curr+1,target,ans);
-        find(root->right,curr+1,target,ans);
+        find(root->left,ans,curr+1,target);
+        find(root->right,ans,curr+1,target);
     }
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
-    vector<int> largestValues(TreeNode* root) {
-        int n=levels(root);
+    vector<int> largestValues(TreeNode* node) {
         vector<int>v;
+        if(node==NULL) return v;
+        int n=levels(node);
+        
         for(int i=1;i<=n;i++){
-            int max=INT_MIN;
-            int curr=1;
-            find(root,curr,i,max);
-            v.push_back(max);
+            int ans=INT_MIN;
+            find(node,ans,1,i);
+            v.push_back(ans);
         }
         return v;
     }
