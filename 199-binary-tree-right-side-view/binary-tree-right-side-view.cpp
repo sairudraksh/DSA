a05e433c3e8917e5a9ce8d9a6254e4211ce7422e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-26 14:49:32 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (40.16%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,25 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void find(TreeNode* root,int curr,int target,int &ans){
-        if(root==NULL) return;
-        if(curr==target) ans=root->val;
-        find(root->left,curr+1,target,ans);
-        find(root->right,curr+1,target,ans);
-    }
     int levels(TreeNode* root){
         if(root==NULL) return 0;
         return 1+max(levels(root->left),levels(root->right));
     }
+    void find(TreeNode* root,int curr,int &target,vector<int>&v){
+        if(root==NULL) return;
+        if(curr==target){
+            v.push_back(root->val);
+            return;
+        }
+        find(root->left,curr+1,target,v);
+        find(root->right,curr+1,target,v);
+    }
     vector<int> rightSideView(TreeNode* root) {
         int n=levels(root);
-        vector<int>v;
-        if(root==NULL) return v;
+        vector<int>ans;
+        if(root==NULL) return ans;
+        
         for(int i=1;i<=n;i++){
-            int ans=0;
-            int curr=1;
-            find(root,curr,i,ans);
-            v.push_back(ans);
+            vector<int>v;
+            find(root,1,i,v);
+            ans.push_back(v[v.size()-1]);
         }
-        return v;
+        return ans;
     }
 };
