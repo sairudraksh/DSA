a96ea68161572117782f143d9ace802b160b09aa
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-26 13:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (87.44%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void find(TreeNode* root,string str,vector<string>&v){
+    vector<string>v;
+    void find(TreeNode* root,string &str){
         if(root==NULL) return;
-        str+=to_string(root->val);
-        if(root->left!=NULL || root->right!=NULL) str+="->";
         if(root->left==NULL && root->right==NULL){
-            v.push_back(str);
-        }
-        find(root->left,str,v);
-        find(root->right,str,v);
-    }
-    vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string>v;
-        string str="";
-        if(root->left==NULL && root->right==NULL){
+            string org=str;
             str+=to_string(root->val);
             v.push_back(str);
-            return v;
+            str=org;
+            return;
         }
-        find(root,str,v);
+        string org=str;
+        str+=to_string(root->val);
+        str+="->";
+        find(root->left,str);
+        find(root->right,str);
+        str=org;
+
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string str="";
+        find(root,str);
         return v;
     }
 };
