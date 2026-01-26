444a59413d6694a7e6b71e72e25be07b795e36fd
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-26 15:37:59 +0530

    Time: 3 ms (45.60%) | Memory: 21 MB (44.37%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -1,23 +1,47 @@
+// class Solution {
+// public:
+//     bool find(TreeNode* root,int curr,int &target){
+//         if(root==NULL) return false;
+//         if(root->left==NULL && root->right==NULL){
+//             curr+=root->val;
+//             if(curr==target) return true;
+//             return false;
+//         }
+//         return find(root->left,curr+root->val,target) ||  find(root->right,curr+root->val,target);
+//     }
+//     bool hasPathSum(TreeNode* root, int targetSum) {
+//         if(root==NULL) return false;
+//         return find(root,0,targetSum);
+//     }
+// };
 class Solution {
 public:
     vector<vector<int>>ans;
-    void find(TreeNode* root,int &targetSum,vector<int>v,int sum){
-        if(root==NULL) return;
-        sum+=root->val;
-        v.push_back(root->val);
+    void find(TreeNode* root,int &curr,int &target,vector<int>&v){
+        if(root==NULL) return ;
         if(root->left==NULL && root->right==NULL){
-            if(sum==targetSum)ans.push_back(v);
+            curr+=root->val;
+            v.push_back(root->val);
+            if(curr==target){
+                ans.push_back(v);
+            }
+            curr-=root->val;
+            v.pop_back();
             return;
         }
 
-        find(root->left,targetSum,v,sum);
-        find(root->right,targetSum,v,sum);
-
+        curr+=root->val;
+        v.push_back(root->val);
+        find(root->left,curr,target,v);
+        find(root->right,curr,target,v);
+        curr-=root->val;
+        v.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if(root==NULL) return ans;
         vector<int>v;
-
-        find(root,targetSum,v,0);
+        int curr=0;
+        find(root,curr,targetSum,v);
         return ans;
     }
 };
