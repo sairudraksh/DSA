ba0c9261525cb836c97018a42572a0a40dc2f668
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-26 15:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (33.26%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    void find(TreeNode* root,int sum,int targetSum,bool &flagg){
-        if(root==NULL) return;
-        sum=sum+root->val;
+    bool find(TreeNode* root,int curr,int &target){
+        if(root==NULL) return false;
         if(root->left==NULL && root->right==NULL){
-            if(sum==targetSum) flagg=true;
-            return;
+            curr+=root->val;
+            if(curr==target) return true;
+            return false;
         }
-        find(root->left,sum,targetSum,flagg);
-        find(root->right,sum,targetSum,flagg);
+        return find(root->left,curr+root->val,target) ||  find(root->right,curr+root->val,target);
+        
     }
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(root==NULL) return false;
-        int sum=0;
-        bool flagg=false;
-        find(root,sum,targetSum,flagg);
-        return flagg;
+        return find(root,0,targetSum);
     }
 };
