cfd19b12a2177ceafd929e33a048a9fa81a8400c
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-01-30 12:03:36 +0530

    Time: 43 ms (5.43%) | Memory: 38.5 MB (5.00%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,55 +1,60 @@
 class Solution {
 public:
+    //int x = stoi(s);
     int n;
-    vector<string> v;
-
-    bool isValid(string &s) {
+    vector<string>v;
+    bool Valid(string &s){
+        int count=0;
+        int m=s.length();
+        for(int i=0;i<m;i++){
+            if(s[i]=='.'){
+                count++;
+            }
+        }
+        return count==3;
+    }
+    bool isValid(string &s){
         if (s.empty()) return false;
         if (s.length() > 1 && s[0] == '0') return false;
         if (s.length() > 3) return false;
-
         int x = stoi(s);
         return x >= 0 && x <= 255;
     }
-
-    void find(string &s, string &str, string &ans, int i, int dot) {
-        if (i == n) {
-            if (dot == 3 && isValid(str)){
-                ans += str;
-                v.push_back(ans);
+    void find(string &s,string &str,string &ans,int i){
+        if(i>=n){
+            if(isValid(str)){
+                string org=ans;
+                if (!ans.empty()) ans+=".";
+                ans+=str;
+                if(Valid(ans)){
+                    v.push_back(ans);
+                }
+                ans=org;
             }
             return;
         }
+        str+=s[i];
+        if(isValid(str)){
+            string org=ans;
+            string org2=str;
 
-        // take current digit
-        str.push_back(s[i]);
+            if (!ans.empty()) ans+=".";
+            ans+=str;
+            str="";
+            find(s,str,ans,i+1);
+            
+            ans=org;
+            str=org2;
 
-        // continue current segment
-        if (isValid(str)) {
-            find(s, str, ans, i + 1, dot);
+            if(str.length()<=3) find(s,str,ans,i+1);
         }
-
-        // cut segment here (add dot)
-        if (isValid(str) && dot < 3) {
-            string oldStr = str;
-            string oldAns = ans;
-
-            ans += str + ".";
-            str = "";
-            find(s, str, ans, i + 1, dot + 1);
-
-            str = oldStr;
-            ans = oldAns;
-        }
-
         str.pop_back();
     }
-
-    vector<string> restoreIpAddresses(string s) {
-        n = s.length();
-        string ans = "";
-        string str = "";
-        find(s, str, ans, 0, 0);
+    vector<string> restoreIpAddresses(string s){
+        n=s.length();
+        string ans="";
+        string str="";
+        find(s,str,ans,0);
         return v;
     }
 };
