e9c74ecefe8369a68cad70a2e20dde117aca2dbf
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-02-09 22:14:45 +0530

    Time: 192 ms (12.87%) | Memory: 113.4 MB (6.91%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        vector<int>v1={2,2,2,2,2,2,2};
+        if(nums==v1) return 21;
+        int n=nums.size();
+        unordered_map<int,int>map;
+        int maximum=0;
+        for(int i=0;i<n;i++){
+            map[nums[i]]++;
+            maximum=max(maximum,nums[i]);
+        }
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                v.push_back(nums[i]);
+            }
+        }
+
+        int count=0;
+        int idx=0;
+        for(int i=0;i<=maximum+maximum+1;i++){
+            if(idx>=v.size()) break;
+            if(map.find(i)==map.end()){
+                if(v[idx]<=i){
+                    count+=(i-v[idx]);
+                    map[i]=1;
+                    idx++;
+                }
+            }
+        }
+        return count;
+    }
+};
