1d0910f07a7e0517553dc28d3f707c99616006b5
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-02-16 18:42:57 +0530

    Time: 45 ms (10.04%) | Memory: 45.7 MB (9.05%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int n;
+    int m;
+    vector<vector<int>>ans;
+    vector<vector<int>>visited;
+    void find(vector<vector<int>>&grid){
+        visited.resize(n+1,vector<int>(m+1,0));
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    q.push({i,j});
+                }
+            }
+        }
+        int count=0;
+        visited.resize(n+1,vector<int>(m+1,0));
+        ans.resize(n,vector<int>(m,0));
+        while(q.size()>0){
+            int size=q.size();
+            for(int l=0;l<size;l++){
+                auto p=q.front();
+                q.pop();
+
+                int i=p.first;
+                int j=p.second;
+                if(grid[i][j]==1 && visited[i][j]==0){
+                    ans[i][j]=count;
+                }
+                visited[i][j]=1;
+                if(i+1<n && visited[i+1][j]==0){
+                    q.push({i+1,j});
+                    
+                }
+                if(j+1<m && visited[i][j+1]==0){
+                    q.push({i,j+1});
+                    
+                }
+                if(i-1>=0 && visited[i-1][j]==0){
+                    q.push({i-1,j});
+                
+                }
+                if(j-1>=0 && visited[i][j-1]==0){
+                    q.push({i,j-1});
+                
+                }
+            }
+            count++;
+        }
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+        find(grid);
+        return ans;
+    }
+};
