5d80b6e89fdd37f579de455e78017ceb7d6a033b
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-02-24 11:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (89.13%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -10,7 +10,7 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
-                if(j==0 || j==i) v[i][j]=1;
+                if(j==0 || i==j) v[i][j]=1;
                 else v[i][j]=v[i-1][j]+v[i-1][j-1];
             }
         }
