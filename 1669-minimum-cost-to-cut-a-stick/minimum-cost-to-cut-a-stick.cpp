13d2b028a7bca2e796fa4e66042db6ac46bd5304
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-03-05 13:40:39 +0530

    Time: 43 ms (9.78%) | Memory: 13.9 MB (78.27%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int find(vector<int>& cuts,int left,int right){
-        if(right-left<=1) return 0;
+    int find(vector<int>&cuts,int left,int right){
+        if(left+1==right || left>=right) return 0;
         if(dp[left][right]!=-1) return dp[left][right];
-        int maximum=INT_MAX;
+        int minimum=INT_MAX;
         for(int i=left+1;i<=right-1;i++){
-            int ans=(cuts[right]-cuts[left])+find(cuts,left,i)+find(cuts,i,right);
-
-            maximum=min(maximum,ans);
+            minimum=min(minimum,(cuts[right]-cuts[left])+find(cuts,left,i)+find(cuts,i,right));
         }
-        return dp[left][right]=maximum;
+        if(minimum==INT_MIN) return dp[left][right]=0;
+        return dp[left][right]=minimum;
     }
     int minCost(int n, vector<int>& cuts) {
-        sort(cuts.begin(),cuts.end());
-        cuts.insert(begin(cuts), 0);
-        cuts.push_back(n);
-        dp.resize(103,vector<int>(103,-1));
+        cuts.push_back(0);
 
+        cuts.push_back(n);
+
+        sort(cuts.begin(),cuts.end());
+        int m=cuts.size();
+        dp.resize(m+1,vector<int>(m+1,-1));
         return find(cuts,0,cuts.size()-1);
     }
 };
