87aa298891ec835778282bfef86892303dbdf6da
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-03-18 17:46:53 +0530

    Time: 43 ms (83.93%) | Memory: 20.5 MB (83.57%) - LeetSync

--- 884-k-similar-strings/k-similar-strings.cpp
+++ 884-k-similar-strings/k-similar-strings.cpp
@@ -3,8 +3,8 @@ public:
     unordered_map<string,int>map;
     int find(string &s1,string &s2,int i){
         if(s1==s2) return 0;
-
         if(i>=s1.length()) return INT_MAX;
+        if(map.find(s1)!=map.end()) return map[s1];
         while(i<s1.length() && s1[i]==s2[i]) i++;
         int minimum=INT_MAX;
         for(int j=i+1;j<s1.length();j++){
@@ -22,7 +22,7 @@ public:
                 swap(s1[i],s1[j]);
             }
         }
-        return minimum;
+        return map[s1]=minimum;
     }
 
     int kSimilarity(string s1, string s2) {
