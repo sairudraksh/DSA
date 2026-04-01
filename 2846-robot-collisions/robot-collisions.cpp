efd6ba1276ca3235ba8b15f73468b8c051266607
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-04-01 10:44:00 +0530

    Time: 120 ms (18.56%) | Memory: 245.7 MB (13.97%) - LeetSync

--- 2846-robot-collisions/robot-collisions.cpp
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& health, string directions) {
         int n=positions.size();
-        stack<pair<pair<int,int>,pair<char,int>>> st1;
-        vector<pair<pair<int,int>,pair<char,int>>> v;
+        stack<pair<pair<int,int>,pair<char,int>>>st1;
+        vector<pair<pair<int,int>,pair<char,int>>>v;
 
         for(int i=0;i<n;i++){
             v.push_back({{positions[i],health[i]},{directions[i],i}});
@@ -15,37 +15,36 @@ public:
             st1.push(v[i]);
         }
 
-        stack<pair<pair<int,int>,pair<char,int>>> st2;
+        stack<pair<pair<int,int>,pair<char,int>>>st2;
 
         st2.push(st1.top());
         st1.pop();
 
         while(st1.size()>0){
             if(st2.size()>0){
+                if(st2.top().second.first=='R' && st1.top().second.first=='L'){
+                    char ch1=st2.top().second.first;
+                    char ch2=st1.top().second.first;
 
-                char ch1 = st2.top().second.first;
-                char ch2 = st1.top().second.first;
+                    auto p1=st1.top().first;
+                    auto p2=st2.top().first;
 
-                if(ch1=='R' && ch2=='L'){
-                    
-                    auto top2 = st2.top(); st2.pop(); // save before pop
-                    auto top1 = st1.top(); st1.pop();
+                    int idx2=st2.top().second.second;
+                    int idx1=st1.top().second.second;
 
-                    int h2 = top2.first.second;
-                    int h1 = top1.first.second;
-
-                    int idx2 = top2.second.second;
-                    int idx1 = top1.second.second;
-
-                    if(h1 == h2){
-                        // both die
+                    if(p1.second==p2.second){
+                        st1.pop();
+                        st2.pop();
                     }
-                    else if(h1 < h2){
-                        st2.push({{top2.first.first,h2-1},{ch1,idx2}});
+                    else if(p1.second<p2.second){
+                        st2.pop();
+                        st1.pop();
+                        st2.push({{p2.first,p2.second-1},{ch1,idx2}});
                     }
                     else{
-                        // current survives → push back to st1 for chain collision
-                        st1.push({{top1.first.first,h1-1},{ch2,idx1}});
+                        st2.pop();
+                        st1.pop();
+                        st1.push({{p1.first,p1.second-1},{ch2,idx1}});
                     }
                 }
                 else{
@@ -58,18 +57,17 @@ public:
                 st1.pop();
             }
         }
-
-        vector<int> ans(n,-1);
+        unordered_map<int,int>map;
         while(st2.size()>0){
-            auto p=st2.top();
-            ans[p.second.second]=p.first.second;
+            auto p=st2.top().second;
+            auto p2=st2.top().first;
+            map[p.second]=p2.second;
             st2.pop();
         }
-
         vector<int>v1;
-        for(int i=0;i<n;i++){
-            if(ans[i]!=-1){
-                v1.push_back(ans[i]);
+        for(int i=0;i<positions.size();i++){
+            if(map.find(i)!=map.end()){
+                v1.push_back(map[i]);
             }
         }
         return v1;
