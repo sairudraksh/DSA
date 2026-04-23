8d31c5bd8bd40fb52f59b950bc419a18aaac73d2
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-04-24 00:07:38 +0530

    Time: 108 ms (23.75%) | Memory: 126.9 MB (23.00%) - LeetSync

---
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {// for reference watch code with mik
+        int n=nums.size();
+
+        unordered_map<int,long long>map1;
+        unordered_map<int,long long>map2;
+        vector<long long>arr1;
+        for(int i=0;i<n;i++){
+            long long no_of_idx=map2[nums[i]];
+            long long sum=map1[nums[i]];
+
+            arr1.push_back(no_of_idx*i-sum);
+
+            map2[nums[i]]++;
+            map1[nums[i]]+=i;
+        }
+        map1.clear();
+        map2.clear();
+
+        vector<long long>arr2;
+
+        for(int i=n-1;i>=0;i--){
+            long long no_of_idx=map2[nums[i]];
+            long long sum=map1[nums[i]];
+
+            arr1[i]+=sum-no_of_idx*i;
+            map2[nums[i]]++;
+            map1[nums[i]]+=i;
+        }
+        return arr1;
+    }
+};
