4a00986b476d5fda070ee8bd9fe97641a6468084
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-05-10 17:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (16.80%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,48 +1,44 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void find(TreeNode* root,int curr,int &target,vector<int>&v){
-        if(root==NULL) return;
-
-        if(curr==target){
-            v.push_back(root->val);
-            return;
-        }
-
-        find(root->left,curr+1,target,v);
-        find(root->right,curr+1,target,v);
-    }
-    int levels(TreeNode* root){
-        if(root==NULL) return 0;
-        return 1+max(levels(root->left),levels(root->right));
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
         if(root==NULL) return ans;
-        int n=levels(root);
-        bool straight=true;
-        for(int i=1;i<=n;i++){
-            vector<int>v;
-            find(root,1,i,v);
-            if(!straight){
-                straight=true;
-                reverse(v.begin(),v.end());
-                ans.push_back(v);
+        queue<TreeNode*>q;
+        bool leftoright=true;
+        q.push(root);
+        while(q.size()>0){
+            int size=q.size();
+            vector<int>v(size);
+            for(int i=0;i<size;i++){
+                auto p=q.front();
+                q.pop();
+
+                int idx;
+
+                if(leftoright){
+                    idx=i;
+                }
+                else{
+                    idx=size-1-i;
+                }
+
+                v[idx]=p->val;
+
+                if(p->left){
+                    q.push(p->left);
+                }
+                if(p->right){
+                    q.push(p->right);
+                }
+            }
+            if(leftoright){
+                leftoright=false;
             }
             else{
-                straight=false;
-                ans.push_back(v);
+                leftoright=true;
             }
+
+            ans.push_back(v);
         }
         return ans;
     }
