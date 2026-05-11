e549d6ce88d048e48611af933931ae63b357900d
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-05-11 13:26:20 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (84.55%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -4,23 +4,24 @@ public:
         int n=nums.size();
         int i=0;
         while(i<n){
-            if(nums[i]<=0 || nums[i]>n){
+            if(nums[i]<=0 || nums[i]>=n){
                 i++;
             }
             else if(nums[i]==i+1){
                 i++;
             }
-            else if(nums[i]!=nums[nums[i]-1]){
-                swap(nums[i],nums[nums[i]-1]);
-            }
-            else{
+            else if(nums[nums[i]-1]==nums[i]){
                 i++;
             }
-        }
+            else{
+                int idx=nums[i]-1;
 
-        for(int k=0;k<n;k++){
-            if(nums[k]!=k+1) return k+1;
+                swap(nums[i],nums[idx]);
+            }
         }
-        return nums[n-1]+1;
+        for(i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1; 
+        }
+        return nums[nums.size()-1]+1;
     }
 };
