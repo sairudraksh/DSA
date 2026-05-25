01eed3e45295b8b991f175dab51adb377a04c3ab
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-05-26 03:06:02 +0530

    Time: 3 ms (41.30%) | Memory: 14 MB (79.84%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
+    void find(int curr,int n,vector<int>&v){
+        if(curr>n) return;
+        if(curr!=0) v.push_back(curr);
+        for(int i=0;i<=9;i++){
+
+            int num=curr*10+i;
+            if(num==0) continue;
+            find(num,n,v);
+
+        }
+    }
     vector<int> lexicalOrder(int n) {
-        vector<string>v;
-
-        for(int i=1;i<=n;i++){
-            string s=to_string(i);
-
-            v.push_back(s);
-        }
-
-        sort(v.begin(),v.end());
-
-        vector<int>ans;
-
-        for(int i=0;i<v.size();i++){
-            int num=stoi(v[i]);
-
-            ans.push_back(num);
-        }
-        return ans;
+        vector<int>v;
+        find(0,n,v);
+        return v;
     }
 };
