2474322171d39b42cebeddd23e0b9588c4d2cce6
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-07-01 10:38:13 +0530

    Time: 476 ms (51.50%) | Memory: 202.5 MB (34.79%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -1,60 +1,64 @@
 class Solution {
 public:
-    int n;
-    int m;
     vector<vector<int>>visited;
-    bool isTrue(int value,vector<vector<int>>& grid,vector<vector<int>>& dist){
+    vector<vector<int>>dist;
+    int n;
+    bool canGo(vector<vector<int>>&grid,int val){
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            for(int j=0;j<n;j++){
                 visited[i][j]=0;
             }
         }
-
-        queue<pair<int,int>>q;
-        if(dist[0][0]<value) return false;
+        if(dist[0][0] < val) return false;
         q.push({0,0});
-
+        visited[0][0]=1;
 
         while(q.size()>0){
             auto p=q.front();
             q.pop();
             int i=p.first;
             int j=p.second;
+
             if(i==n-1 && j==n-1) return true;
-            if(i+1<n && visited[i+1][j]==0 && dist[i+1][j]>=value){
-                q.push({i+1,j});
+
+            if(i+1<n && visited[i+1][j]==0 && dist[i+1][j]>=val){
                 visited[i+1][j]=1;
+                q.push({i+1,j});
             }
-            if(j+1<n && visited[i][j+1]==0 && dist[i][j+1]>=value){
-                q.push({i,j+1});
+
+            if(j+1<n && visited[i][j+1]==0 && dist[i][j+1]>=val){
                 visited[i][j+1]=1;
+                q.push({i,j+1});
             }
-            if(i-1>=0 && visited[i-1][j]==0 && dist[i-1][j]>=value){
-                q.push({i-1,j});
+
+            if(i-1>=0 && visited[i-1][j]==0 && dist[i-1][j]>=val){
                 visited[i-1][j]=1;
+                q.push({i-1,j});
             }
-            if(j-1>=0 && visited[i][j-1]==0 && dist[i][j-1]>=value){
-                q.push({i,j-1});
+
+            if(j-1>=0 && visited[i][j-1]==0 && dist[i][j-1]>=val){
                 visited[i][j-1]=1;
+                q.push({i,j-1});
             }
         }
         return false;
     }
     int maximumSafenessFactor(vector<vector<int>>& grid) {
         n=grid.size();
-        m=grid[0].size();
-        vector<vector<int>>dist(n,vector<int>(m,-1));
-
-
+        dist.resize(n+1,vector<int>(n+1,-1));
+        visited.resize(n+1,vector<int>(n+1));
         queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
                     q.push({i,j});
                     dist[i][j]=0;
                 }
             }
         }
+
+
         while(q.size()>0){
             auto p=q.front();
             q.pop();
@@ -65,26 +69,31 @@ public:
                 dist[i+1][j]=dist[i][j]+1;
                 q.push({i+1,j});
             }
+
             if(j+1<n && dist[i][j+1]==-1){
                 dist[i][j+1]=dist[i][j]+1;
                 q.push({i,j+1});
             }
+
             if(i-1>=0 && dist[i-1][j]==-1){
                 dist[i-1][j]=dist[i][j]+1;
                 q.push({i-1,j});
             }
+
             if(j-1>=0 && dist[i][j-1]==-1){
                 dist[i][j-1]=dist[i][j]+1;
                 q.push({i,j-1});
             }
         }
+
         int lo=0;
         int hi=n;
+
         int ans=0;
-        visited.resize(n,vector<int>(m,0));
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(isTrue(mid,grid,dist)){
+
+            if(canGo(grid,mid)){
                 ans=mid;
                 lo=mid+1;
             }
