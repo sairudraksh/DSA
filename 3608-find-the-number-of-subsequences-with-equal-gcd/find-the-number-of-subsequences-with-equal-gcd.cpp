cf2d6080f54d1cfbb0a9844d6982c93ded8a40cf
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-07-16 01:45:47 +0530

    Time: 1236 ms (20.40%) | Memory: 58.8 MB (63.19%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int n;
     long long mod=1e9+7;
-    long long dp[202][202][202];
-    long long find(vector<int>&nums,int i,int gcd1,int gcd2){
+    int dp[202][202][202];
+    int find(vector<int>&nums,int i,int gcd1,int gcd2){
         if(i>=n){
             if(gcd1!=-1 && gcd1==gcd2){
                 return 1;
@@ -20,11 +20,10 @@ public:
         if(gcd2==-1) newgcd2=nums[i];
         else newgcd2=gcd(nums[i],gcd2);
 
-        return dp[i][gcd1+1][gcd2+1]=(find(nums,i+1,newgcd1,gcd2) + find(nums,i+1,gcd1,newgcd2) + find(nums,i+1,gcd1,gcd2))%mod;
+        return dp[i][gcd1+1][gcd2+1]=(find(nums,i+1,newgcd1,gcd2)%mod + find(nums,i+1,gcd1,newgcd2)%mod + find(nums,i+1,gcd1,gcd2)%mod)%mod;
     }
     int subsequencePairCount(vector<int>& v) {
         n=v.size();
-        
         memset(dp,-1,sizeof(dp));
         return find(v,0,-1,-1)%mod;
     }
