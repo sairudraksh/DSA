3f5e77b40adbdac7eb41b46371f7c4c76e54c05e
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-08-07 11:32:26 +0530

    Time: 22 ms (88.62%) | Memory: 17.4 MB (47.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -3,54 +3,25 @@ public:
     vector<vector<int>>visited;
     int n;
     int m;
-    int find(vector<vector<char>>& grid,int l,int r){
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]=='0' || visited[i][j]==1) return;
+        visited[i][j]=1;
 
-        queue<pair<int,int>>q;
-        q.push({l,r});
-        visited[l][r]=1;
-
-        while(q.size()>0){
-            auto p=q.front();
-            q.pop();
-            int i=p.first;
-            int j=p.second;
-            if(i+1<n && grid[i+1][j]!='0' && visited[i+1][j]==0){
-                q.push({i+1,j});
-                visited[i+1][j]=1;
-            }
-
-            if(i-1>=0 && grid[i-1][j]!='0' && visited[i-1][j]==0){
-                q.push({i-1,j});
-                visited[i-1][j]=1;
-            }
-
-            if(j+1<m && grid[i][j+1]!='0' && visited[i][j+1]==0){
-                q.push({i,j+1});
-                visited[i][j+1]=1;
-            }
-
-            if(j-1>=0 && grid[i][j-1]!='0' && visited[i][j-1]==0){
-                q.push({i,j-1});
-                visited[i][j-1]=1;
-            }
-        }
-        return 1;
+        dfs(grid,i+1,j),dfs(grid,i,j+1),dfs(grid,i-1,j),dfs(grid,i,j-1);
     }
     int numIslands(vector<vector<char>>& grid) {
         n=grid.size();
         m=grid[0].size();
-
         visited.resize(n+1,vector<int>(m+1,0));
-        
-        int count=0;
+        int result=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1' && visited[i][j]==0){
-                    int a=find(grid,i,j);
-                    count+=a;
+                    result+=1;
+                    dfs(grid,i,j);
                 }
             }
         }
-        return count;
+        return result;
     }
 };
