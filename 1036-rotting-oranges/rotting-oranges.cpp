9b55b57a4e7079c529c3db67c64fb9a29675b489
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-08-10 00:28:43 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (39.48%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,63 +1,55 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-
+    int n;
+    int m;
+    vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+    int bfs(vector<vector<int>>& grid){
         queue<pair<int,int>>q;
-        int count1=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 || grid[i][j]==2){
-                    count1++;
-                }
-            }
-        }
-        int count2=0;
+        int goodOranges=0;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
-                    count2++;
                 }
+                
             }
         }
-        int ans=0;
-
+        int count=0;
         while(q.size()>0){
             int size=q.size();
-
             for(int l=0;l<size;l++){
                 auto p=q.front();
                 q.pop();
-
                 int i=p.first;
                 int j=p.second;
-                if(i+1<n && grid[i+1][j]!=0 && grid[i+1][j]!=2){
-                    grid[i+1][j]=2;
-                    count2++;
-                    q.push({i+1,j});
-                }
-                if(i-1>=0 && grid[i-1][j]!=0 && grid[i-1][j]!=2){
-                    grid[i-1][j]=2;
-                    count2++;
-                    q.push({i-1,j});
-                }
-                if(j+1<m && grid[i][j+1]!=0 && grid[i][j+1]!=2){
-                    grid[i][j+1]=2;
-                    count2++;
-                    q.push({i,j+1});
-                }
-                if(j-1>=0 && grid[i][j-1]!=0 && grid[i][j-1]!=2){
-                    grid[i][j-1]=2;
-                    count2++;
-                    q.push({i,j-1});
+                
+                for(int t=0;t<dir.size();t++){
+                    int newi=i+dir[t][0];
+                    int newj=j+dir[t][1];
+
+                    if(newi<n && newj<m && newi>=0 && newj>=0 && grid[newi][newj]==1){
+                        q.push({newi,newj});
+                        grid[newi][newj]=2;
+                      
+                    }
                 }
             }
 
-            if(q.size()>0) ans++;
+            if(size>0) count++;
+        }  
+        return count;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        n=grid.size();
+        m=grid[0].size();
+        int a=bfs(grid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
+            }
         }
-        if(count1!=count2) return -1;
-        return ans;
+        if(a==0) return a;
+        return a-1;
     }
 };
