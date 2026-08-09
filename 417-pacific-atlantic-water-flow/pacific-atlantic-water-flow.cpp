b5fc3184cb45999da7b95fca986ec56e4f974e84
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-08-09 21:41:22 +0530

    Time: 4 ms (77.22%) | Memory: 24 MB (34.60%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-    vector<vector<int>>directions={{0,1},{1,0},{-1,0},{0,-1}};
     int n;
     int m;
-    void find(vector<vector<int>>& heights,int i,int j,vector<vector<int>>&visited,int prev){
-        if(i>=m || j>=n || i<0 || j<0) return;
+    vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+    void find(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&visited,int prev){
+        if(i>=n || j>=m || i<0 || j<0 || visited[i][j]==1) return;
 
-        if(visited[i][j] || heights[i][j]<prev){
-            return;
-        }
+        if(grid[i][j]<prev) return; 
 
         visited[i][j]=1;
 
-        for(auto &dir:directions){
-            int l=dir[0];
-            int r=dir[1];
-            find(heights,i+l,j+r,visited,heights[i][j]);
+        for(int p=0;p<dir.size();p++){
+            int first=dir[p][0];
+            int second=dir[p][1];
+
+            find(grid,i+first,j+second,visited,grid[i][j]);
         }
+
     }
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        m=heights.size();
-        n=heights[0].size();
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& grid){
+        n=grid.size();
+        m=grid[0].size();
+        vector<vector<int>>pacificVisited;
+        vector<vector<int>>atlanticVisited;
+        pacificVisited.resize(n+1,vector<int>(m+1,0));
+        atlanticVisited.resize(n+1,vector<int>(m+1,0));
 
-        vector<vector<int>>pasificVisited(m+1,vector<int>(n+1,0));
-        
-        vector<vector<int>>AtlanticVisited(m+1,vector<int>(n+1,0));
-
+        for(int i=0;i<m;i++){
+            find(grid,0,i,pacificVisited,INT_MIN);
+            find(grid,n-1,i,atlanticVisited,INT_MIN);
+        }
 
         for(int i=0;i<n;i++){
-            find(heights,0,i,pasificVisited,INT_MIN);
-            find(heights,m-1,i,AtlanticVisited,INT_MIN);
-        }
-        for(int i=0;i<m;i++){
-            find(heights,i,0,pasificVisited,INT_MIN);
-            find(heights,i,n-1,AtlanticVisited,INT_MIN);
+            find(grid,i,0,pacificVisited,INT_MIN);
+            find(grid,i,m-1,atlanticVisited,INT_MIN);
         }
         vector<vector<int>>ans;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(pasificVisited[i][j]==1 && AtlanticVisited[i][j]==1) ans.push_back({i,j});
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(pacificVisited[i][j]==1 && atlanticVisited[i][j]==1){
+                    ans.push_back({i,j});
+                }
             }
         }
         return ans;
