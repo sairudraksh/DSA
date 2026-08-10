51fe812619a584225d072e968a0051ee50689e49
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-08-10 18:45:11 +0530

    Time: 12 ms (81.45%) | Memory: 37.7 MB (46.21%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -2,57 +2,46 @@ class Solution {
 public:
     int n;
     int m;
-    vector<vector<int>>ans;
-    vector<vector<int>>visited;
-    void find(vector<vector<int>>&grid){
+    vector<vector<int>>dir={{0,1},{1,0},{-1,0},{0,-1}};
+    void find(vector<vector<int>>& grid){
+        vector<vector<int>>visited;
         visited.resize(n+1,vector<int>(m+1,0));
         queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==0){
                     q.push({i,j});
+                    visited[i][j]=1;
                 }
             }
         }
-        int count=0;
-        visited.resize(n+1,vector<int>(m+1,0));
-        ans.resize(n,vector<int>(m,0));
+        int count=1;
         while(q.size()>0){
             int size=q.size();
             for(int l=0;l<size;l++){
                 auto p=q.front();
                 q.pop();
-
                 int i=p.first;
                 int j=p.second;
-                if(grid[i][j]==1 && visited[i][j]==0){
-                    ans[i][j]=count;
-                }
-                visited[i][j]=1;
-                if(i+1<n && visited[i+1][j]==0){
-                    q.push({i+1,j});
-                    
-                }
-                if(j+1<m && visited[i][j+1]==0){
-                    q.push({i,j+1});
-                    
-                }
-                if(i-1>=0 && visited[i-1][j]==0){
-                    q.push({i-1,j});
-                
-                }
-                if(j-1>=0 && visited[i][j-1]==0){
-                    q.push({i,j-1});
-                
+
+                for(int t=0;t<dir.size();t++){
+                    int newi=i+dir[t][0];
+                    int newj=j+dir[t][1];
+
+                    if(newi<n && newj<m && newi>=0 && newj>=0 && visited[newi][newj]==0){
+                        q.push({newi,newj});
+                        grid[newi][newj]=count;
+                        visited[newi][newj]=1;
+                    }
                 }
             }
             count++;
         }
     }
-    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
-        find(grid);
-        return ans;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        n=mat.size();
+        m=mat[0].size();
+        find(mat);
+        return mat;
     }
 };
