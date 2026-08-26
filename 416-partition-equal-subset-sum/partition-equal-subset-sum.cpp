b84d0997744be4024c9b55ee9eb32ebe733e60be
Sai Rudraksh Rana <rudrakshrana42@gmail.com>
2026-08-26 23:02:57 +0530

    Time: 59 ms (90.93%) | Memory: 111.4 MB (13.56%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int n;
     vector<vector<int>>dp;
-    bool find(vector<int>&nums,int &target,int i,int sum){
+    bool find(vector<int>&nums,int &target,int sum,int i){
         if(sum==target) return true;
-        if(i>=nums.size()) return false;
         if(sum>target) return false;
+        if(i>=n) return false;
         if(dp[i][sum]!=-1) return dp[i][sum];
-        return dp[i][sum]=find(nums,target,i+1,sum+nums[i]) || find(nums,target,i+1,sum);
+        return dp[i][sum]=find(nums,target,sum+nums[i],i+1) || find(nums,target,sum,i+1);
     }
     bool canPartition(vector<int>& nums) {
-        n=nums.size();
         int sum=0;
+        n=nums.size();
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
+
         if(sum%2!=0) return false;
+
         int target=sum/2;
         dp.resize(n+1,vector<int>(target+1,-1));
         return find(nums,target,0,0);
